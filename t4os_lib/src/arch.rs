9342661774ae1f51816444ccb689862b46291e07
Yuuki Takano <ytakanoster@gmail.com>
2023-01-19 11:04:29 +0900

    fix dependencies for linux
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- t4os_lib/src/arch.rs
+++ t4os_lib/src/arch.rs
@@ -1,19 +1,19 @@
-#[cfg(all(target_arch = "aarch64", not(feature = "linux")))]
+#[cfg(all(target_arch = "aarch64", not(target_os = "linux")))]
 pub mod aarch64;
 
-#[cfg(all(target_arch = "x86_64", not(feature = "linux")))]
+#[cfg(all(target_arch = "x86_64", not(target_os = "linux")))]
 pub mod x86_64;
 
-#[cfg(feature = "linux")]
+#[cfg(target_os = "linux")]
 pub mod linux;
 
-#[cfg(all(target_arch = "aarch64", not(feature = "linux")))]
+#[cfg(all(target_arch = "aarch64", not(target_os = "linux")))]
 use self::aarch64 as arch_mod;
 
-#[cfg(all(target_arch = "x86_64", not(feature = "linux")))]
+#[cfg(all(target_arch = "x86_64", not(target_os = "linux")))]
 use self::x86_64 as arch_mod;
 
-#[cfg(feature = "linux")]
+#[cfg(target_os = "linux")]
 use self::linux as arch_mod;
 
 pub(crate) type ArchDelay = arch_mod::delay::ArchDelay;
