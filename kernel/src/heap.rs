841391fa43f9d38acbc7cb2b45da8760e40270ca
Sun Jia <sun1011jacobi@gmail.com>
2023-02-02 11:09:57 +0900

    add tlsf as kmalloc

--- kernel/src/heap.rs
+++ kernel/src/heap.rs
@@ -7,15 +7,43 @@ pub enum InitErr {
     FailedToMapPage,
 }
 
+
+static mut HEAP: [MaybeUninit<u8>; 256 * 1024 * 1024] = [MaybeUninit::uninit(); 256 * 1024 * 1024];
+ 
 pub fn init() {
     // Initialize memory allocator.
-    unsafe {
-        ALLOC.init(
-            crate::config::HEAP_START as usize,
-            crate::config::HEAP_SIZE as usize,
-        );
-    }
+    
+    let mut node = MCSNode::new();
+    let mut guard = TALLOC.0.lock(&mut node);
+    unsafe { guard.insert_free_block(&mut HEAP); } 
+
 }
 
+use core::mem::MaybeUninit;
+use synctools::mcs::{MCSLock, MCSNode};
+use rlsf::Tlsf;
+use core::ptr::NonNull;
+use core::alloc::GlobalAlloc;
+
+
+struct Allocator<'pool, FLBitmap, SLBitmap, const FLLEN: usize, const SLLEN: usize>
+(MCSLock<Tlsf<'pool, FLBitmap, SLBitmap, FLLEN, SLLEN>>);
+
 #[global_allocator]
-static mut ALLOC: memac::Allocator<memac::buddy::Buddy256M> = memac::Allocator::new();
+static TALLOC : Allocator<'_, u16, u16, 12, 16> = Allocator(MCSLock::new(Tlsf::new()));
+
+
+
+unsafe impl<'pool> GlobalAlloc for Allocator<'_, u16, u16, 12, 16> {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        let mut node = MCSNode::new();
+        let mut guard = TALLOC.0.lock(&mut node);
+        guard.allocate(layout).unwrap().as_mut()
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        let mut node = MCSNode::new();
+        let mut guard = TALLOC.0.lock(&mut node);
+        guard.deallocate(NonNull::new_unchecked(ptr), layout.align());
+    }
+}
