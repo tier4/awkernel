53dd0887b060edb332e5808f94e9572f815ab9b8
Yuuki Takano <ytakanoster@gmail.com>
2023-02-06 17:07:13 +0900

    add test and comments to anydict.rs
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- t4os_async_lib/src/anydict.rs
+++ t4os_async_lib/src/anydict.rs
@@ -1,24 +1,29 @@
-use core::any::Any;
+//! Dictionary mapping from string to any value.
 
 use alloc::{borrow::Cow, boxed::Box, collections::BTreeMap};
+use core::any::Any;
 
+/// Return value of `AnyDict`.
 pub(super) enum AnyDictResult<T> {
-    Ok(T),
-    TypeError,
-    None,
+    Ok(T),     // Found.
+    TypeError, // Type of stored value and requested type are incompatible.
+    None,      // Not found.
 }
 
+/// Dictionary from `Cow<'static, str>` to any value.
 pub(super) struct AnyDict {
     dict: BTreeMap<Cow<'static, str>, Box<dyn Any>>,
 }
 
 impl AnyDict {
+    /// Create a new `AnyDict`.
     pub(super) const fn new() -> Self {
         Self {
             dict: BTreeMap::new(),
         }
     }
 
+    /// Insert a key value pair.
     pub(super) fn insert<T>(&mut self, key: Cow<'static, str>, value: T)
     where
         T: Any,
@@ -26,6 +31,9 @@ impl AnyDict {
         self.dict.insert(key, Box::new(value));
     }
 
+    /// Get a value stored before.
+    /// If there is no value of the key, `AnyDictResult::None` will be returned.
+    /// If the types are incompatible, `AnyDictResult::TypeError` will be returned.
     pub(super) fn get<T>(&self, key: &str) -> AnyDictResult<&T>
     where
         T: Any,
@@ -41,6 +49,9 @@ impl AnyDict {
         }
     }
 
+    /// Get a value stored before.
+    /// If there is no value of the key, `AnyDictResult::None` will be returned.
+    /// If the types are incompatible, `AnyDictResult::TypeError` will be returned.
     pub(super) fn get_mut<T>(&mut self, key: &str) -> AnyDictResult<&mut T>
     where
         T: Any,
@@ -56,7 +67,46 @@ impl AnyDict {
         }
     }
 
+    /// Remove the value mapped by `key`.
     pub(super) fn remove(&mut self, key: &str) -> Option<Box<dyn Any>> {
         self.dict.remove(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::string::ToString;
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut dict = AnyDict::new();
+
+        dict.insert("key1".into(), 10 as u64);
+        dict.insert("key2".into(), "Hello".to_string());
+
+        let AnyDictResult::Ok(value1) = dict.get::<u64>("key1".into()) else { panic!() };
+        let AnyDictResult::Ok(value2) = dict.get::<String>("key2".into()) else { panic!() };
+
+        assert_eq!(*value1, 10);
+        assert_eq!(*value2, "Hello");
+
+        assert!(matches!(
+            dict.get::<u64>("key2".into()),
+            AnyDictResult::TypeError
+        ));
+
+        assert!(matches!(
+            dict.get::<String>("key1".into()),
+            AnyDictResult::TypeError
+        ));
+
+        assert!(matches!(
+            dict.get::<u64>("key2".into()),
+            AnyDictResult::TypeError
+        ));
+
+        assert!(matches!(dict.get::<()>("key3".into()), AnyDictResult::None))
     }
 }
