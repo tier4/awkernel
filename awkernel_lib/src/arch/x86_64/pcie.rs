bee323aad39e47dd107363fa61d631a5dbdfff6b
Sun Jia <sun1011jacobi@gmail.com>
2023-05-18 13:02:25 +0900

    X86, LIB: search for device during booting.

--- awkernel_lib/src/arch/x86_64/pcie.rs
+++ awkernel_lib/src/arch/x86_64/pcie.rs
@@ -1,13 +1,44 @@
-use crate::arch::x86_64::{AcpiMapper, AcpiTables, OffsetPageTable, PageAllocator};
-use acpi::PciConfigRegions;
+use crate::arch::x86_64::{mmu::map_to, AcpiMapper, AcpiTables, PageAllocator};
+use acpi::{mcfg::McfgEntry, PciConfigRegions};
+use core::ptr::read_volatile;
 
-pub fn init(
+use x86_64::structures::paging::{OffsetPageTable, PageTableFlags, PhysFrame};
+
+pub fn init<T>(
     acpi: &AcpiTables<AcpiMapper>,
     page_table: &mut OffsetPageTable<'static>,
-    page_allocator: &mut PageAllocator,
-) {
+    page_allocator: &mut PageAllocator<T>,
+) where
+    T: Iterator<Item = PhysFrame> + Send,
+{
     let pcie_info = PciConfigRegions::new(acpi).unwrap();
-    for device in pcie_info.list_all() {
-        log::info!("{:?}", device);
+    for segment in pcie_info.list_all() {
+        log::info!("{:?}", segment);
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE;
+        let mut pa_start = segment.base_address() as usize;
+        let pa_end =  pa_start + 0x10000000;
+        while pa_start  <  pa_end {
+            unsafe {
+                map_to(pa_start, pa_start, flags, page_table, page_allocator);
+            }
+            pa_start += 4096;
+        }
+        
+        search_devices(&segment);
+    }
+}
+
+fn search_devices(segment: &McfgEntry) {
+    for bus in segment.buses() {
+        for dev in 0..(1 << 5) {
+            for func in 0..(1 << 3) {
+                let offset = (bus as u64) << 20 | dev << 15| func << 12;
+                let addr = segment.base_address() + offset;
+                let id = unsafe { read_volatile(addr as *const u32) };
+                if id != !0 {
+                    log::info!("found device with id {:#x} at {:#x}", id, addr);
+                }
+            }
+        }
     }
 }
