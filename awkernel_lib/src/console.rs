0286fcb2fac57ad96bcd09c863b663baa7074226
Yuuki Takano <ytakanoster@gmail.com>
2023-06-01 11:48:35 +0900

    LIB: add console.rs
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

---
+++ awkernel_lib/src/console.rs
@@ -0,0 +1,16 @@
+pub trait Console {
+    /// Print raw data.
+    unsafe fn raw_puts(data: &str);
+
+    /// Enable the serial port.
+    fn enable(&mut self);
+
+    /// Disable the serial port.
+    fn disable(&mut self);
+
+    /// Enable the reception interrupt.
+    fn enable_recv_interrupt(&mut self);
+
+    /// Disable the reception interrupt.
+    fn disable_recv_interrupt(&mut self);
+}
