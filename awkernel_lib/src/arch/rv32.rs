f0c6b29fdd4055bd0e8efc066b87ad54f8c1f8b5
PeterWrighten <peterwrighten@gmail.com>
2023-06-20 16:42:32 +0900

    add virtual memory related mods in rv32.rs

--- awkernel_lib/src/arch/rv32.rs
+++ awkernel_lib/src/arch/rv32.rs
@@ -2,4 +2,8 @@ pub mod context;
 pub(super) mod cpu;
 pub(super) mod delay;
 pub(super) mod interrupt;
+
+// Virtual Memory
 pub(super) mod memory;
+pub(super) mod address;
+pub(super) mod page_table;
