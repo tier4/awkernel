8962d8d36b615a6fd0673a4917365a6326220d35
kobayu <yutaro.kobayashi@tier4.jp>
2023-06-29 15:45:35 +0900

    feat: wake_task

--- awkernel_async_lib/src/scheduler/prioritized_round_robin.rs
+++ awkernel_async_lib/src/scheduler/prioritized_round_robin.rs
@@ -1,33 +1,59 @@
-use alloc::sync::Arc;
-use awkernel_lib::sync::mutex::Mutex;
-
-use crate::task::TaskList;
+//! A prioritized round robin scheduler.
 
 use super::{Scheduler, SchedulerType, Task};
+use crate::task;
+use alloc::{collections::VecDeque, sync::Arc};
+use awkernel_lib::sync::mutex::{MCSNode, Mutex};
 
 pub struct PrioritizedRoundRobinScheduler {
     data: Mutex<Option<PrioritizedRoundRobinData>>, // Run queue.
 }
 
 struct PrioritizedRoundRobinData {
-    queue: TaskList,
+    queue: VecDeque<Arc<Task>>,
 }
 
 impl PrioritizedRoundRobinData {
     fn new() -> Self {
         Self {
-            queue: TaskList::new(),
+            queue: VecDeque::new(),
         }
     }
 }
 
 impl Scheduler for PrioritizedRoundRobinScheduler {
     fn wake_task(&self, task: Arc<Task>) {
-        todo!()
+        let mut node = MCSNode::new();
+        let mut data = self.data.lock(&mut node);
+
+        if data.is_none() {
+            *data = Some(PrioritizedRoundRobinData::new());
+        }
+
+        let data = data.as_mut().unwrap();
+
+        // Put the state in queue.
+        let mut node = MCSNode::new();
+        let mut task_info = task.info.lock(&mut node);
+
+        // If the task is in queue or the state is Terminated, it must not be enqueued.
+        if task_info.in_queue
+            || matches!(
+                task_info.state,
+                task::State::Terminated | task::State::Panicked
+            )
+        {
+            return;
+        }
+
+        data.queue.push_back(task.clone());
+
+        // The task is in queue.
+        task_info.in_queue = true;
     }
 
     fn get_next(&self) -> Option<Arc<Task>> {
-        todo!()
+        todo!("PrioritizedRoundRobinScheduler::get_next");
     }
 
     fn scheduler_name(&self) -> SchedulerType {
