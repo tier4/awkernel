b7966048cdf36100bf1d2148cade0e146a68ae79
kobayu <yutaro.kobayashi@tier4.jp>
2023-08-22 15:19:48 +0900

    feat: perf-eval

--- awkernel_async_lib/src/task.rs
+++ awkernel_async_lib/src/task.rs
@@ -9,6 +9,9 @@
 #[cfg(not(feature = "no_preempt"))]
 mod preempt;
 
+#[cfg(feature = "std")]
+use std::time::Instant;
+
 use crate::{
     delay::wait_microsec,
     scheduler::{self, get_scheduler, Scheduler, SchedulerType},
@@ -277,6 +280,10 @@ fn get_next_task() -> Option<Arc<Task>> {
 }
 
 pub fn run_main() {
+    #[cfg(feature = "std")]
+    let mut exe_time: Vec<u128> = Vec::new();
+    #[cfg(feature = "std")]
+    let mut switch_time: Vec<u128> = Vec::new();
     loop {
         if let Some(task) = get_next_task() {
             #[cfg(not(feature = "no_preempt"))]
@@ -338,28 +345,54 @@ pub fn run_main() {
                 }
 
                 // Invoke a task.
-                catch_unwind(|| {
-                    #[cfg(all(
-                        any(target_arch = "aarch64", target_arch = "x86_64"),
-                        not(feature = "std")
-                    ))]
-                    {
-                        awkernel_lib::interrupt::enable();
-                    }
+                #[cfg(feature = "std")]
+                {
+                    catch_unwind(|| {
+                        let start = Instant::now();
 
-                    #[allow(clippy::let_and_return)]
-                    let result = guard.poll_unpin(&mut ctx);
+                        #[allow(clippy::let_and_return)]
+                        let result = guard.poll_unpin(&mut ctx);
 
-                    #[cfg(all(
-                        any(target_arch = "aarch64", target_arch = "x86_64"),
-                        not(feature = "std")
-                    ))]
-                    {
-                        awkernel_lib::interrupt::disable();
-                    }
+                        let elapsed = start.elapsed();
+                        exe_time.push(elapsed.as_nanos());
+                        if exe_time.len() % 100000 == 0 {
+                            log::info!(
+                                "CPU#{:?} exe_time: min = {:?} [ns], avg = {:?} [ns], max = {:?} [ns]",
+                                cpu_id = awkernel_lib::cpu::cpu_id(),
+                                exe_time.iter().min().unwrap(),
+                                exe_time.iter().sum::<u128>() / exe_time.len() as u128,
+                                exe_time.iter().max().unwrap(),
+                            );
+                            exe_time.clear();
+                        }
+                        result
+                    })
+                }
+                #[cfg(not(feature = "std"))]
+                {
+                    catch_unwind(|| {
+                        #[cfg(all(
+                            any(target_arch = "aarch64", target_arch = "x86_64"),
+                            not(feature = "std")
+                        ))]
+                        {
+                            awkernel_lib::interrupt::enable();
+                        }
 
-                    result
-                })
+                        #[allow(clippy::let_and_return)]
+                        let result = guard.poll_unpin(&mut ctx);
+
+                        #[cfg(all(
+                            any(target_arch = "aarch64", target_arch = "x86_64"),
+                            not(feature = "std")
+                        ))]
+                        {
+                            awkernel_lib::interrupt::disable();
+                        }
+
+                        result
+                    })
+                }
             };
 
             // If the primary memory allocator is available, it will be used.
@@ -378,13 +411,41 @@ pub fn run_main() {
 
             match result {
                 Ok(Poll::Pending) => {
-                    // The task has not been terminated yet.
-                    info.state = State::Waiting;
+                    #[cfg(feature = "std")]
+                    {
+                        let start = std::time::Instant::now();
 
-                    if info.need_sched {
-                        info.need_sched = false;
-                        drop(info);
-                        task.clone().wake();
+                        info.state = State::Waiting;
+
+                        if info.need_sched {
+                            info.need_sched = false;
+                            drop(info);
+                            task.clone().wake();
+                        }
+
+                        let elapsed = start.elapsed();
+                        switch_time.push(elapsed.as_nanos());
+
+                        if switch_time.len() % 100000 == 0 {
+                            log::info!(
+                                "CPU#{:?} switch_time: min = {:?} [ns], avg = {:?} [ns], max = {:?} [ns]",
+                                awkernel_lib::cpu::cpu_id(),
+                                switch_time.iter().min().unwrap(),
+                                switch_time.iter().sum::<u128>() / switch_time.len() as u128,
+                                switch_time.iter().max().unwrap(),
+                            );
+                            switch_time.clear();
+                        }
+                    }
+                    #[cfg(not(feature = "std"))]
+                    {
+                        info.state = State::Waiting;
+
+                        if info.need_sched {
+                            info.need_sched = false;
+                            drop(info);
+                            task.clone().wake();
+                        }
                     }
                 }
                 Ok(Poll::Ready(result)) => {
