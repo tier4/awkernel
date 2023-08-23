d527e443bd994df980814870331a2b42ecbc1adf
Yuuki Takano <ytakanoster@gmail.com>
2023-08-23 15:22:40 +0900

    App, Raspi: add test code for GPIO output
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- applications/test_rpi_hal/src/lib.rs
+++ applications/test_rpi_hal/src/lib.rs
@@ -1,14 +1,27 @@
+#![no_std]
+
+use awkernel_drivers::hal::rpi::gpio::GpioPin;
+use core::time::Duration;
+use embedded_hal::digital::v2::OutputPin;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn run_rpi_hal() {
+    let pin = GpioPin::new(26).unwrap();
+    let mut gpio26 = pin.into_output();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let mut flag = true;
+    loop {
+        if flag {
+            gpio26.set_high().unwrap();
+        } else {
+            gpio26.set_low().unwrap();
+        }
+
+        flag = !flag;
+
+        awkernel_async_lib::sleep(Duration::from_secs(1)).await;
     }
 }
--- userland/src/lib.rs
+++ userland/src/lib.rs
@@ -128,5 +128,13 @@ pub async fn main() -> Result<(), Cow<'static, str>> {
         .await;
     }
 
+    #[cfg(feature = "raspi")]
+    spawn(
+        "test_rpi_hal".into(),
+        test_rpi_hal::run_rpi_hal(),
+        SchedulerType::FIFO,
+    )
+    .await;
+
     Ok(())
 }
