eaf35ae4dd96634111d2ad78b6f06159675a0816
Yuuki Takano <ytakanoster@gmail.com>
2023-11-20 00:47:24 +0900

    DRIVERS, PCIe: fix to use igb instead of e1000
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- awkernel_drivers/src/pcie.rs
+++ awkernel_drivers/src/pcie.rs
@@ -481,8 +481,8 @@ impl PCIeInfo {
     {
         match self.vendor {
             pcie_id::INTEL_VENDOR_ID => {
-                if net::e1000::match_device(self.vendor, self.id) {
-                    return net::e1000::attach(self, dma_offset, page_table, page_allocator);
+                if net::igb::match_device(self.vendor, self.id) {
+                    return net::igb::attach(self, dma_offset, page_table, page_allocator);
                 }
             }
             _ => (),
