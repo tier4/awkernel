0c76961b20fa9012169ae1a9a1f3ed2efd2b8ab1
bopeng-saitama <bo.peng@tier4.jp>
2024-04-20 15:55:35 +0900

    fix: remove raspi5_uart
    Signed-off-by: bopeng-saitama <bo.peng@tier4.jp>

--- kernel/src/arch/aarch64/bsp/config.rs
+++ kernel/src/arch/aarch64/bsp/config.rs
@@ -7,5 +7,4 @@ pub use super::raspi5::config::*;
 #[cfg(feature = "aarch64_virt")]
 pub use super::aarch64_virt::config::*;
 
-#[cfg(not(feature = "raspi5"))]
 pub const UART_BAUDRATE: usize = 115200;
