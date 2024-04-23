2fb64c3fe5209a3ff2a119671740c8f4080b8706
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-04-23 11:54:52 +0900

    refacor: deleted cfg fuature raspi5

--- kernel/src/arch/aarch64/kernel_main.rs
+++ kernel/src/arch/aarch64/kernel_main.rs
@@ -54,7 +54,6 @@ pub unsafe extern "C" fn kernel_main(device_tree_base: usize) -> ! {
 /// 5. Enable heap allocator.
 /// 6. Board specific initialization (Interrupt controller, etc).
 unsafe fn primary_cpu(device_tree_base: usize) {
-    #[cfg(not(feature = "raspi5"))]
     let device_tree = load_device_tree(device_tree_base);
     #[cfg(not(feature = "raspi5"))]
     let mut initializer = super::bsp::SoCInitializer::new(device_tree, device_tree_base);
