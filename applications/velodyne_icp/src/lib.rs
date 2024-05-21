0c6f73a456b244b695dedff58f615630ed0f36a5
IshitaTakeshi <ishitah.takeshi@gmail.com>
2024-05-21 18:30:12 +0900

    Add a thread to draw lines

--- applications/velodyne_icp/src/lib.rs
+++ applications/velodyne_icp/src/lib.rs
@@ -5,10 +5,15 @@ extern crate alloc;
 use awkernel_async_lib::scheduler::SchedulerType;
 use awkernel_async_lib::net::udp::UdpConfig;
 use awkernel_async_lib::net::IpAddr;
-use velodyne_driver::{Point, PointCloudCalculator, parse_config};
+use velodyne_driver::{PointCloudCalculator, parse_config};
 use velodyne_driver::{N_SEQUENCES, CHANNELS_PER_SEQUENCE, PointProcessor};
 use awkernel_async_lib::pubsub::{create_publisher, create_subscriber};
 use awkernel_async_lib::pubsub;
+use awkernel_lib::graphics;
+
+use embedded_graphics::pixelcolor::Rgb888;
+use embedded_graphics::prelude::RgbColor;
+use embedded_graphics::geometry;
 
 use core::borrow::Borrow;
 use alloc::boxed::Box;
@@ -35,7 +40,7 @@ impl Default for Scan {
 }
 
 impl PointProcessor for Scan {
-    fn process(&mut self, sequence_index: usize, channel: usize, point: &Point) {
+    fn process(&mut self, sequence_index: usize, channel: usize, point: &velodyne_driver::Point) {
         assert!(!self.is_full());
         if channel != 1 {
             return;
@@ -57,33 +62,64 @@ impl Scan {
     }
 }
 
+const POSITION_TOPIC: &str = "position";
 const SCAN_TOPIC: &str = "scan";
 
-fn run_icp(transform: &icp::Transform, src_scan: &Arc<Box<Scan>>, dst_scan: &Arc<Box<Scan>>) -> icp::Transform {
+fn run_icp(src_scan: &Arc<Box<Scan>>, dst_scan: &Arc<Box<Scan>>) -> icp::Transform {
+    let prior = icp::Transform::identity();
     let src: &Box<Scan> = src_scan.borrow();
     let dst: &Box<Scan> = dst_scan.borrow();
-    icp::icp_2dscan(transform, &src.points, &dst.points)
+    icp::icp_2dscan(&prior, &src.points, &dst.points)
 }
 
 async fn icp() {
-    let subscriber =
+    let scan_subscriber =
         create_subscriber::<Arc<Box<Scan>>>(SCAN_TOPIC.into(), pubsub::Attribute::default()).unwrap();
+    let position_publisher =
+        create_publisher::<Arc<Box<(f64, f64)>>>(POSITION_TOPIC.into(), pubsub::Attribute::default()).unwrap();
 
     let mut transform = icp::Transform::identity();
     let mut maybe_reference_scan = None;
     loop {
-        let scan_message: pubsub::Data<Arc<Box<Scan>>> = subscriber.recv().await;
+        let scan_message: pubsub::Data<Arc<Box<Scan>>> = scan_subscriber.recv().await;
         let scan: Arc<Box<Scan>> = scan_message.data;
 
         if let Some(ref reference_scan) = maybe_reference_scan {
-           transform = run_icp(&transform, &scan, &reference_scan);
+           let dtransform = run_icp(&scan, &reference_scan);
+           transform = transform * dtransform;
+           let xy = (transform.t[0], transform.t[1]);
+           position_publisher.send(Arc::new(Box::new(xy))).await;
         }
 
-        log::info!("transform.R:");
-        log::info!("{:?}", transform.rot);
-        log::info!("transform.t:");
-        log::info!("{:?}", transform.t);
         maybe_reference_scan = Some(scan);
+    }
+}
+
+async fn draw() {
+    let position_subscriber =
+        create_subscriber::<Arc<Box<(f64, f64)>>>(POSITION_TOPIC.into(), pubsub::Attribute::default()).unwrap();
+    let mut maybe_last = None;
+
+    loop {
+        // Fill the screen with black
+        graphics::fill(&Rgb888::BLACK);
+
+        let m: pubsub::Data<Arc<Box<(f64, f64)>>> = position_subscriber.recv().await;
+        let (x, y): (f64, f64) = **(m.data);
+
+        let curr = geometry::Point::new(x as i32, y as i32);
+
+        if let Some(last) = maybe_last {
+            let _ = graphics::line(
+                last,
+                curr,
+                &Rgb888::RED,
+                1);
+        }
+
+        maybe_last = Some(curr);
+
+        graphics::flush();
     }
 }
 
@@ -108,7 +144,7 @@ async fn receive_scan_packets() {
     let config = parse_config(vlp16_config_str).unwrap();
     let calculator = PointCloudCalculator::new(&config);
 
-    let publisher =
+    let scan_publisher =
         create_publisher::<Arc<Box<Scan>>>(SCAN_TOPIC.into(), pubsub::Attribute::default()).unwrap();
 
     let mut scan = Box::new(Scan::default());
@@ -119,14 +155,15 @@ async fn receive_scan_packets() {
         scan.increment();
 
         if scan.is_full() {
-            publisher.send(Arc::new(scan)).await;
+            scan_publisher.send(Arc::new(scan)).await;
             scan = Box::new(Scan::default());
         }
     }
 }
 
 pub async fn run() {
-    awkernel_async_lib::spawn("udp".into(), receive_scan_packets(), SchedulerType::FIFO).await;
+    awkernel_async_lib::spawn("draw".into(), draw(), SchedulerType::FIFO).await;
+    awkernel_async_lib::spawn("receive_scan_packets".into(), receive_scan_packets(), SchedulerType::FIFO).await;
     awkernel_async_lib::spawn("icp".into(), icp(), SchedulerType::FIFO).await;
 }
 
