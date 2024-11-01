fb523dfb5cae5882b7fa2bb56858daa2f4d3d6b0
Yuuki Takano <ytakanoster@gmail.com>
2024-11-01 11:22:13 +0900

    remove unsed import
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- awkernel_async_lib/src/task.rs
+++ awkernel_async_lib/src/task.rs
@@ -25,7 +25,7 @@ use awkernel_lib::{
     unwind::catch_unwind,
 };
 use core::{
-    sync::atomic::{AtomicU32, AtomicUsize, Ordering},
+    sync::atomic::{AtomicU32, Ordering},
     task::{Context, Poll},
 };
 use futures::{
