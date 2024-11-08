c5eef63ac6826abc8c000d8e6fc9c8e3eee1ba18
Yuuki Takano <yuuki.takano@tier4.jp>
2024-11-08 13:53:41 +0900

    chore: make DVFS APIs inline
    Signed-off-by: Yuuki Takano <yuuki.takano@tier4.jp>

--- awkernel_lib/src/dvfs.rs
+++ awkernel_lib/src/dvfs.rs
@@ -10,16 +10,19 @@ pub trait Dvfs {
 }
 
 /// Fix the frequency of the current CPU.
+#[inline(always)]
 pub fn fix_freq(freq: u64) {
     crate::arch::ArchImpl::fix_freq(freq);
 }
 
 /// Get the maximum frequency of the current CPU.
+#[inline(always)]
 pub fn get_max_freq() -> u64 {
     crate::arch::ArchImpl::get_max_freq()
 }
 
 /// Get the frequency of the current CPU.
+#[inline(always)]
 pub fn get_curr_freq() -> u64 {
     crate::arch::ArchImpl::get_curr_freq()
 }
