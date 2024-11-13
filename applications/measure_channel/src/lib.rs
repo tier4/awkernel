00362e9795605f0f8c27e0ed22dffbafdf794753
Yuuki Takano <ytakanoster@gmail.com>
2024-11-13 16:53:45 +0900

    measure RTT of channel and print the 99th percentile
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- applications/measure_channel/src/lib.rs
+++ applications/measure_channel/src/lib.rs
@@ -2,11 +2,13 @@
 
 extern crate alloc;
 
+use core::time::Duration;
+
 use alloc::format;
 use awkernel_async_lib::{channel::bounded, scheduler::SchedulerType, spawn, uptime_nano};
 
 pub async fn run() {
-    for i in 0..10 {
+    for i in 0..100 {
         let (tx1, rx1) = bounded::new::<()>(bounded::Attribute::default());
         let (tx2, rx2) = bounded::new::<()>(bounded::Attribute::default());
 
@@ -14,8 +16,13 @@ pub async fn run() {
             format!("{i}-server").into(),
             async move {
                 loop {
-                    rx1.recv().await.unwrap();
-                    tx2.send(()).await.unwrap();
+                    if rx1.recv().await.is_err() {
+                        return;
+                    }
+
+                    if tx2.send(()).await.is_err() {
+                        return;
+                    }
                 }
             },
             SchedulerType::RR,
@@ -25,19 +32,35 @@ pub async fn run() {
         spawn(
             format!("{i}-client").into(),
             async move {
-                loop {
-                    let start = uptime_nano();
-                    tx1.send(()).await.unwrap();
-                    rx2.recv().await.unwrap();
-                    let end = uptime_nano();
-
-                    let elapsed = end - start;
-
-                    log::debug!("{elapsed} [ns]");
-                }
+                client_task(tx1, rx2).await;
             },
             SchedulerType::RR,
         )
         .await;
     }
+}
+
+const NUM_TRIAL: usize = 10000;
+
+async fn client_task(tx1: bounded::Sender<()>, rx2: bounded::Receiver<()>) {
+    let mut result = alloc::vec::Vec::with_capacity(NUM_TRIAL);
+
+    for _ in 0..NUM_TRIAL {
+        let start = uptime_nano();
+        tx1.send(()).await.unwrap();
+        rx2.recv().await.unwrap();
+        let end = uptime_nano();
+
+        let elapsed = end - start;
+
+        result.push(elapsed);
+    }
+
+    awkernel_async_lib::sleep(Duration::from_secs(3)).await;
+
+    result.sort();
+
+    let p99 = result[NUM_TRIAL / 100 * 99]; // 99th percentile
+
+    log::debug!("99th percentile =  {p99} [ns]");
 }
