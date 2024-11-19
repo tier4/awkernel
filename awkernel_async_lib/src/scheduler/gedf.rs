b8974abd2279704ab23d87be4a81e33fddaffe09
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-11-19 12:37:46 +0900

    refactor: cmp
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- awkernel_async_lib/src/scheduler/gedf.rs
+++ awkernel_async_lib/src/scheduler/gedf.rs
@@ -29,12 +29,8 @@ impl PartialEq for GEDFTask {
 
 impl Ord for GEDFTask {
     fn cmp(&self, other: &Self) -> core::cmp::Ordering {
-        match self
-            .absolute_deadline
-            .cmp(&other.absolute_deadline)
-            .reverse()
-        {
-            core::cmp::Ordering::Equal => self.wake_time.cmp(&other.wake_time).reverse(),
+        match other.absolute_deadline.cmp(&self.absolute_deadline) {
+            core::cmp::Ordering::Equal => other.wake_time.cmp(&self.wake_time),
             other => other,
         }
     }
