ebc3d1db1e2ed1815062e059f82cadb5c7f04bbe
Yuuki Takano <ytakanoster@gmail.com>
2024-11-26 17:38:10 +0900

    update test
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- applications/test_bravo_rwlock/src/lib.rs
+++ applications/test_bravo_rwlock/src/lib.rs
@@ -8,21 +8,24 @@ use alloc::sync::Arc;
 use awkernel_async_lib::{scheduler::SchedulerType, spawn};
 use awkernel_lib::{cpu::num_cpu, delay::wait_microsec, sync::bravo_rwlock::BravoRwLock};
 
-/// This test confirms that RR scheduler does not cause a starvation.
-/// There are more tasks than num_cpu(), but all tasks will be run infinite times.
 pub async fn run() {
     let data = Arc::new(BravoRwLock::new(false));
 
     for i in 0..(num_cpu() - 1) {
         let d = data.clone();
+
+        // prepare reader tasks
         spawn(
             "reader".into(),
             async move {
                 loop {
-                    // let guard = d.read();
-                    // if *guard {
-                    //     log::debug!("invalid, no={i}");
-                    // }
+                    {
+                        let guard = d.read();
+                        if *guard {
+                            // data must be false during the read operation
+                            log::debug!("invalid, no={i}");
+                        }
+                    }
 
                     awkernel_async_lib::r#yield().await;
                 }
@@ -32,15 +35,16 @@ pub async fn run() {
         .await;
     }
 
+    // prepare writer task
     spawn(
         "writer".into(),
         async move {
             loop {
                 {
-                    // let mut guard = data.write();
-                    // *guard = true;
-                    wait_microsec(50);
-                    // *guard = false;
+                    let mut guard = data.write();
+                    *guard = true; // set true during the write operation
+                    wait_microsec(2000);
+                    *guard = false;
                 }
 
                 awkernel_async_lib::sleep(Duration::from_millis(10)).await;
