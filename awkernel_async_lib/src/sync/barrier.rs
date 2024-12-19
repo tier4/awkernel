5f360a1a80f2b0ea573696310ccf6883451846e6
r1ru <riruoda@gmail.com>
2024-12-19 13:16:19 +0900

    Handles the case where Barrier::wait is called more than the specified number of times

--- awkernel_async_lib/src/sync/barrier.rs
+++ awkernel_async_lib/src/sync/barrier.rs
@@ -1,11 +1,13 @@
 use crate::pubsub::{self, Attribute, Publisher, Subscriber};
+use alloc::{vec, vec::Vec};
 use core::sync::atomic::AtomicUsize;
 
 /// A barrier enables multiple threads to synchronize the beginning of some computation.
 pub struct Barrier {
     count: AtomicUsize,
+    num_threads: usize,
     tx: Publisher<()>,
-    rx: Subscriber<()>,
+    rxs: Vec<Subscriber<()>>,
 }
 
 /// `BarrierWaitResult` is returned by `Barrier::wait` when all threads in `Barrier` have rendezvoused.
@@ -19,33 +21,38 @@ impl BarrierWaitResult {
 
 impl Barrier {
     /// Creates a new barrier that can block a given number of threads.
-    pub fn new(count: usize) -> Self {
+    pub fn new(n: usize) -> Self {
         let attr = Attribute {
             queue_size: 1,
             ..Attribute::default()
         };
         let (tx, rx) = pubsub::create_pubsub(attr);
 
+        let mut rxs = vec![rx.clone(); n - 2];
+        rxs.push(rx);
+
         Self {
-            count: AtomicUsize::new(count),
+            num_threads: n,
+            count: AtomicUsize::new(0),
             tx,
-            rx,
+            rxs,
         }
     }
 
     /// Blocks the current thread until all threads have redezvoused here.
     /// A single (arbitrary) thread will receive `BarrierWaitResult(true)` when returning fron this function, and other threads will receive `BarrierWaitResult(false)`.
     pub async fn wait(&self) -> BarrierWaitResult {
-        if self
+        let count = self
             .count
-            .fetch_sub(1, core::sync::atomic::Ordering::Relaxed)
-            == 1
-        {
+            .fetch_add(1, core::sync::atomic::Ordering::Relaxed);
+        if count < self.num_threads - 1 {
+            self.rxs[count].recv().await;
+            BarrierWaitResult(false)
+        } else {
+            // Safety: count mut be set to 0 before calling Sender::poll, as it switches to a task waiting to receive.
+            self.count.store(0, core::sync::atomic::Ordering::Relaxed);
             self.tx.send(()).await;
             BarrierWaitResult(true)
-        } else {
-            self.rx.clone().recv().await;
-            BarrierWaitResult(false)
         }
     }
 }
@@ -69,15 +76,22 @@ mod tests {
             let num_leaders = num_leaders.clone();
             let task = async move {
                 num_waits.fetch_add(1, Ordering::Relaxed);
+
                 if barrier.wait().await.is_reader() {
                     num_leaders.fetch_add(1, Ordering::Relaxed);
                 }
+                // Verify that Barrier synchronizes the specified number of threads
                 assert_eq!(num_waits.load(Ordering::Relaxed), 10);
+
+                // it is safe to call Barrier::wait more than count times
+                barrier.wait().await;
             };
+
             tasks.spawn(task);
         }
         tasks.run();
 
+        // Vefify that only one thread receives BarrierWaitResult(true)
         assert_eq!(num_leaders.load(Ordering::Relaxed), 1);
     }
 }
