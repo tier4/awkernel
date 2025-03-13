06c5fb01384153bb51c65f588bb261774875c1a9
Yuuki Takano <ytakanoster@gmail.com>
2025-03-13 14:22:16 +0900

    fix(logger): enable buffered logger
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- kernel/src/arch/std_common/kernel_main.rs
+++ kernel/src/arch/std_common/kernel_main.rs
@@ -8,7 +8,10 @@ use libc::c_void;
 #[no_mangle]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
     // Initialize.
-    unsafe { crate::config::init() };
+    unsafe {
+        crate::config::init();
+        awkernel_lib::logger::init();
+    }
     awkernel_lib::arch::std_common::init();
     awkernel_lib::arch::std_common::init_per_thread(0);
     console::init();
