86d2716f238c3588e4e5795ff0fce875261faa84
Yuuki Takano <ytakanoster@gmail.com>
2025-03-18 11:39:36 +0900

    fix(igc): remove unnecessary fields
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- awkernel_drivers/src/pcie/intel/igc/i225.rs
+++ awkernel_drivers/src/pcie/intel/igc/i225.rs
@@ -1,20 +1,13 @@
-use super::igc_hw::{IgcHw, IgcMacOperations, IgcNvmOperations, IgcOperations, IgcPhyOperations};
-use crate::pcie::PCIeInfo;
+use super::igc_hw::{IgcMacOperations, IgcNvmOperations, IgcOperations, IgcPhyOperations};
 
-pub(super) struct I225Flash {
-    info: PCIeInfo,
-    hw: IgcHw,
-}
+pub(super) struct I225Flash;
 
 impl IgcOperations for I225Flash {}
 impl IgcMacOperations for I225Flash {}
 impl IgcPhyOperations for I225Flash {}
 impl IgcNvmOperations for I225Flash {}
 
-pub(super) struct I225NoFlash {
-    info: PCIeInfo,
-    hw: IgcHw,
-}
+pub(super) struct I225NoFlash;
 
 impl IgcOperations for I225NoFlash {}
 impl IgcMacOperations for I225NoFlash {}
