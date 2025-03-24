e5f4c074938291a84cd14a94f2ec179ae74bc8af
Yuuki Takano <ytakanoster@gmail.com>
2025-03-24 17:46:01 +0900

    clippy
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- awkernel_lib/src/arch/x86_64/msr.rs
+++ awkernel_lib/src/arch/x86_64/msr.rs
@@ -13,7 +13,7 @@ pub(super) const IA32_HWP_ACTIVITY_WINDOW: u64 = 0x3ff << 32;
 pub(super) const _IA32_HWP_REQUEST_ENERGY_PERFORMANCE_PREFERENCE: u64 = 0xff << 24;
 pub(super) const IA32_HWP_DESIRED_PERFORMANCE: u64 = 0xff << 16;
 pub(super) const IA32_HWP_REQUEST_MAXIMUM_PERFORMANCE: u64 = 0xff << 8;
-pub(super) const IA32_HWP_MINIMUM_PERFORMANCE: u64 = 0xff << 0;
+pub(super) const IA32_HWP_MINIMUM_PERFORMANCE: u64 = 0xff;
 
 pub(super) const MSR_IA32_PM_ENABLE: u32 = 0x770;
 pub(super) const MSR_IA32_HWP_CAPABILITIES: u32 = 0x771;
@@ -22,14 +22,20 @@ pub(super) const MSR_IA32_HWP_REQUEST: u32 = 0x774;
 
 // MSR IA32_HWP_CAPABILITIES
 pub(super) const IA32_HWP_CAPABILITIES_HIGHEST_PERFORMANCE: fn(u64) -> u8 =
-    |x| (((x) >> 0) & 0xff) as u8;
+    |x| ((x >> 0) & 0xff) as u8;
 pub(super) const IA32_HWP_CAPABILITIES_GUARANTEED_PERFORMANCE: fn(u64) -> u8 =
-    |x| (((x) >> 8) & 0xff) as u8;
+    |x| ((x >> 8) & 0xff) as u8;
 pub(super) const IA32_HWP_CAPABILITIES_EFFICIENT_PERFORMANCE: fn(u64) -> u8 =
-    |x| (((x) >> 16) & 0xff) as u8;
+    |x| ((x >> 16) & 0xff) as u8;
 pub(super) const IA32_HWP_CAPABILITIES_LOWEST_PERFORMANCE: fn(u64) -> u8 =
-    |x| (((x) >> 24) & 0xff) as u8;
+    |x| ((x >> 24) & 0xff) as u8;
 
+/// Read 64 bits msr register.
+/// If the `msr` is not supported on your environments, this function returns `false`.
+///
+/// # Safety
+///
+/// The caller must ensure that this read operation has no unsafe side effects.
 pub unsafe fn rdmsr_safe(msr: &Msr) -> Option<u64> {
     let _int_guard = crate::interrupt::InterruptGuard::new();
 
@@ -47,6 +53,12 @@ pub unsafe fn rdmsr_safe(msr: &Msr) -> Option<u64> {
     }
 }
 
+/// Write 64 bits to msr register.
+/// If the `msr` is not supported on your environments, this function returns `false`.
+///
+/// # Safety
+///
+/// The caller must ensure that this write operation has no unsafe side effects.
 pub unsafe fn wrmsr_safe(msr: &mut Msr, value: u64) -> bool {
     let _int_guard = crate::interrupt::InterruptGuard::new();
 
