bc5c2cb56b31d5dd46c1ca6223e9b67462bd9c8a
kobayu858 <yutaro.kobayashi@tier4.jp>
2025-03-24 17:47:33 +0900

    refactor: adaption to merge
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- awkernel_async_lib/src/dag.rs
+++ awkernel_async_lib/src/dag.rs
@@ -1,16 +1,218 @@
 pub mod graph;
 
 use alloc::{
-    collections::{btree_map, BTreeMap},
+    borrow::Cow,
+    boxed::Box,
+    collections::{btree_map, BTreeMap, BTreeSet},
     sync::Arc,
+    vec::Vec,
 };
 use awkernel_lib::sync::mutex::{MCSNode, Mutex};
+use core::{future::Future, pin::Pin, time::Duration};
+
+use crate::dag::graph::{direction::Direction, NodeIndex};
+use crate::{
+    scheduler::SchedulerType, spawn_periodic_reactor, spawn_reactor, MultipleReceiver,
+    MultipleSender, VectorToPublishers, VectorToSubscribers,
+};
 
 static DAGS: Mutex<Dags> = Mutex::new(Dags::new()); // Set of DAGs.
 
+#[derive(Debug, Clone)]
+pub struct NodeInfo {
+    task_id: u32,
+    subscribe_topics: Vec<Cow<'static, str>>,
+    publish_topics: Vec<Cow<'static, str>>,
+}
+
 pub struct Dag {
     pub id: u32,
-    pub graph: Mutex<graph::Graph<u32, u32>>, //TODO: Change to attribute
+    pub graph: Mutex<graph::Graph<NodeInfo, u32>>, //TODO: Change to edge attribute
+    pending_tasks: Mutex<Vec<PendingTask>>,
+}
+
+struct PendingTask {
+    node_idx: NodeIndex,
+    func: Box<dyn FnOnce() -> Pin<Box<dyn Future<Output = u32> + Send>> + Send>,
+}
+
+impl Dag {
+    pub fn get_id(&self) -> u32 {
+        self.id
+    }
+
+    fn add_node_with_topic_edges(
+        &self,
+        subscribe_topic_names: &[Cow<'static, str>],
+        publish_topic_names: &[Cow<'static, str>],
+    ) -> NodeIndex {
+        let add_node_info = NodeInfo {
+            task_id: 0, // Temporary task_id
+            subscribe_topics: subscribe_topic_names.to_vec(),
+            publish_topics: publish_topic_names.to_vec(),
+        };
+
+        let mut node = MCSNode::new();
+        let mut graph = self.graph.lock(&mut node);
+        let add_node_idx = graph.add_node(add_node_info);
+
+        for node_idx in graph.node_indices() {
+            if node_idx != add_node_idx {
+                if let Some(node_info) = graph.node_weight(node_idx).cloned() {
+                    for subscribe_topic in subscribe_topic_names {
+                        if node_info.publish_topics.contains(subscribe_topic) {
+                            graph.add_edge(node_idx, add_node_idx, 0);
+                        }
+                    }
+                    for publish_topic in publish_topic_names {
+                        if node_info.subscribe_topics.contains(publish_topic) {
+                            graph.add_edge(add_node_idx, node_idx, 0);
+                        }
+                    }
+                }
+            }
+        }
+        add_node_idx
+    }
+
+    pub async fn spawn_reactor<F, Args, Ret>(
+        &self,
+        reactor_name: Cow<'static, str>,
+        f: F,
+        subscribe_topic_names: Vec<Cow<'static, str>>,
+        publish_topic_names: Vec<Cow<'static, str>>,
+        sched_type: SchedulerType,
+    ) where
+        F: Fn(
+                <Args::Subscribers as MultipleReceiver>::Item,
+            ) -> <Ret::Publishers as MultipleSender>::Item
+            + Send
+            + 'static,
+        Args: VectorToSubscribers,
+        Ret: VectorToPublishers,
+        Ret::Publishers: Send,
+        Args::Subscribers: Send,
+    {
+        let node_idx = self.add_node_with_topic_edges(&subscribe_topic_names, &publish_topic_names);
+
+        let mut node = MCSNode::new();
+        let mut pending_tasks = self.pending_tasks.lock(&mut node);
+
+        pending_tasks.push(PendingTask {
+            node_idx,
+            func: Box::new(move || {
+                Box::pin(async move {
+                    spawn_reactor::<F, Args, Ret>(
+                        reactor_name.clone(),
+                        f,
+                        subscribe_topic_names.clone(),
+                        publish_topic_names.clone(),
+                        sched_type,
+                    )
+                    .await
+                })
+            }),
+        });
+    }
+
+    pub async fn spawn_periodic_reactor<F, Ret>(
+        &self,
+        reactor_name: Cow<'static, str>,
+        f: F,
+        publish_topic_names: Vec<Cow<'static, str>>,
+        sched_type: SchedulerType,
+        period: Duration,
+    ) where
+        F: Fn() -> <Ret::Publishers as MultipleSender>::Item + Send + 'static,
+        Ret: VectorToPublishers,
+        Ret::Publishers: Send,
+    {
+        let node_idx = self.add_node_with_topic_edges(&Vec::new(), &publish_topic_names);
+
+        let mut node = MCSNode::new();
+        let mut pending_tasks = self.pending_tasks.lock(&mut node);
+
+        pending_tasks.push(PendingTask {
+            node_idx,
+            func: Box::new(move || {
+                Box::pin(async move {
+                    spawn_periodic_reactor::<F, Ret>(
+                        reactor_name.clone(),
+                        f,
+                        publish_topic_names.clone(),
+                        sched_type,
+                        period,
+                    )
+                    .await
+                })
+            }),
+        });
+    }
+
+    fn is_weakly_connected(&self, graph: &graph::Graph<NodeInfo, u32>) -> bool {
+        if graph.node_count() == 0 {
+            return false;
+        }
+
+        let mut visited: BTreeSet<NodeIndex> = BTreeSet::new();
+        let mut stack: Vec<NodeIndex> = Vec::new();
+
+        if let Some(start_node) = graph.node_indices().next() {
+            stack.push(start_node);
+            visited.insert(start_node);
+        }
+
+        while let Some(node_idx) = stack.pop() {
+            for neighbor in graph.neighbors_undirected(node_idx) {
+                if !visited.contains(&neighbor) {
+                    visited.insert(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+
+        visited.len() == graph.node_count()
+    }
+
+    fn is_cycle(&self, graph: &graph::Graph<NodeInfo, u32>) -> bool {
+        let mut visited: BTreeSet<NodeIndex> = BTreeSet::new();
+        let mut stack: Vec<NodeIndex> = Vec::new();
+
+        for node_idx in graph.node_indices() {
+            if !visited.contains(&node_idx) && self.dfs(graph, node_idx, &mut visited, &mut stack) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn dfs(
+        &self,
+        graph: &graph::Graph<NodeInfo, u32>,
+        node_idx: NodeIndex,
+        visited: &mut BTreeSet<NodeIndex>,
+        stack: &mut Vec<NodeIndex>,
+    ) -> bool {
+        if stack.contains(&node_idx) {
+            return true;
+        }
+        if visited.contains(&node_idx) {
+            return false;
+        }
+
+        visited.insert(node_idx);
+        stack.push(node_idx);
+
+        for neighbor in graph.neighbors_directed(node_idx, Direction::Outgoing) {
+            if self.dfs(graph, neighbor, visited, stack) {
+                return true;
+            }
+        }
+
+        stack.pop();
+        false
+    }
 }
 
 /// DAGs.
@@ -40,6 +242,7 @@ impl Dags {
                 let dag = Arc::new(Dag {
                     id,
                     graph: Mutex::new(graph::Graph::new()),
+                    pending_tasks: Mutex::new(Vec::new()),
                 });
 
                 e.insert(dag.clone());
@@ -67,4 +270,110 @@ pub fn get_dag(id: u32) -> Option<Arc<Dag>> {
     dags.id_to_dag.get(&id).cloned()
 }
 
-// TODO: Implementation of API to build DAGs from Reactor
+pub async fn finish_create_dags(dags: &[Arc<Dag>]) {
+    for dag in dags {
+        {
+            let mut graph_node = MCSNode::new();
+            let graph = dag.graph.lock(&mut graph_node);
+            if !dag.is_weakly_connected(&graph) {
+                panic!("DAG ID {} is not weakly connected", dag.get_id());
+            }
+            if dag.is_cycle(&graph) {
+                panic!("DAG ID {} is cycle", dag.get_id());
+            }
+        }
+
+        let mut node = MCSNode::new();
+        let pending_tasks: Vec<_> = dag.pending_tasks.lock(&mut node).drain(..).collect();
+
+        for task in pending_tasks {
+            let task_id = (task.func)().await;
+            let mut graph_node = MCSNode::new();
+            let mut graph = dag.graph.lock(&mut graph_node);
+            if let Some(node_info) = graph.node_weight_mut(task.node_idx) {
+                node_info.task_id = task_id;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn create_node_info(node_id: u32) -> NodeInfo {
+        NodeInfo {
+            task_id: node_id,
+            subscribe_topics: Vec::new(),
+            publish_topics: Vec::new(),
+        }
+    }
+
+    #[test]
+    fn test_is_weakly_connected_true() {
+        let dag = create_dag();
+
+        let mut node = MCSNode::new();
+        let mut graph = dag.graph.lock(&mut node);
+
+        let a = graph.add_node(create_node_info(1));
+        let b = graph.add_node(create_node_info(2));
+        let c = graph.add_node(create_node_info(3));
+
+        graph.add_edge(a, b, 0);
+        graph.add_edge(b, c, 0);
+
+        assert_eq!(dag.is_weakly_connected(&graph), true);
+    }
+
+    #[test]
+    fn test_is_weakly_connected_false() {
+        let dag = create_dag();
+
+        let mut node = MCSNode::new();
+        let mut graph = dag.graph.lock(&mut node);
+
+        let a = graph.add_node(create_node_info(1));
+        let b = graph.add_node(create_node_info(2));
+        graph.add_node(create_node_info(3));
+
+        graph.add_edge(a, b, 0);
+
+        assert_eq!(dag.is_weakly_connected(&graph), false);
+    }
+
+    #[test]
+    fn test_is_cycle_true() {
+        let dag = create_dag();
+
+        let mut node = MCSNode::new();
+        let mut graph = dag.graph.lock(&mut node);
+
+        let a = graph.add_node(create_node_info(1));
+        let b = graph.add_node(create_node_info(2));
+        let c = graph.add_node(create_node_info(3));
+
+        graph.add_edge(a, b, 0);
+        graph.add_edge(b, c, 0);
+        graph.add_edge(c, a, 0);
+
+        assert_eq!(dag.is_cycle(&graph), true);
+    }
+
+    #[test]
+    fn test_is_cycle_false() {
+        let dag = create_dag();
+
+        let mut node = MCSNode::new();
+        let mut graph = dag.graph.lock(&mut node);
+
+        let a = graph.add_node(create_node_info(1));
+        let b = graph.add_node(create_node_info(2));
+        let c = graph.add_node(create_node_info(3));
+
+        graph.add_edge(a, b, 0);
+        graph.add_edge(b, c, 0);
+
+        assert_eq!(dag.is_cycle(&graph), false);
+    }
+}
