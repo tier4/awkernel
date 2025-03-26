08ca3c5d0a73a553fdca897fd58a0d46415b78cd
kobayu858 <129580202+kobayu858@users.noreply.github.com>
2025-03-26 18:15:16 +0900

    feat: building a DAG structure from reactor (#327)
    * feat: build graph from reactor
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * feat: validation of DAG
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * feat: spwan of DAG tasks
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * feat: deleted extra validation
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * test: add unit test
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: cargo fmt
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: clippy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: create_dag
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: dag api
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: clippy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: rename fn
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: conflict
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: conflict
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: adaption to merge
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * test: use assert_eq
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: clippy
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: return error instead of using panic
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: minor correction
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: use petgraph algo
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: rename func to spawn
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: globalization of pending_task
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: edge connetion
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: validate separate
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * fix: cargo fmt
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * docs: add description
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * feat: add pending_task validate
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: remove the use of cloned()
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: remove derive of Nodeinfo
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    * refactor: change order of verification
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>
    
    ---------
    
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- awkernel_async_lib/src/dag.rs
+++ awkernel_async_lib/src/dag.rs
@@ -1,18 +1,175 @@
-pub mod graph;
+mod graph;
 mod unionfind;
 mod visit;
 
+use crate::{
+    dag::graph::{
+        algo::{connected_components, is_cyclic_directed},
+        NodeIndex,
+    },
+    scheduler::SchedulerType,
+    spawn_periodic_reactor, spawn_reactor, MultipleReceiver, MultipleSender, VectorToPublishers,
+    VectorToSubscribers,
+};
 use alloc::{
+    borrow::Cow,
+    boxed::Box,
     collections::{btree_map, BTreeMap},
     sync::Arc,
+    vec::Vec,
 };
 use awkernel_lib::sync::mutex::{MCSNode, Mutex};
+use core::{future::Future, pin::Pin, time::Duration};
 
 static DAGS: Mutex<Dags> = Mutex::new(Dags::new()); // Set of DAGs.
+static PENDING_TASKS: Mutex<BTreeMap<u32, Vec<PendingTask>>> = Mutex::new(BTreeMap::new()); // key: dag_id
+
+pub enum DagError {
+    NotWeaklyConnected(u32),
+    ContainsCycle(u32),
+    MissingPendingTasks(u32),
+}
+
+struct PendingTask {
+    node_idx: NodeIndex,
+    spawn: Box<dyn FnOnce() -> Pin<Box<dyn Future<Output = u32> + Send>> + Send>,
+}
+
+impl PendingTask {
+    fn new<F>(node_idx: NodeIndex, spawn_fn: F) -> Self
+    where
+        F: FnOnce() -> Pin<Box<dyn Future<Output = u32> + Send>> + Send + 'static,
+    {
+        Self {
+            node_idx,
+            spawn: Box::new(spawn_fn),
+        }
+    }
+}
+
+pub struct NodeInfo {
+    task_id: u32,
+    subscribe_topics: Vec<Cow<'static, str>>,
+    publish_topics: Vec<Cow<'static, str>>,
+}
 
 pub struct Dag {
     pub id: u32,
-    pub graph: Mutex<graph::Graph<u32, u32>>, //TODO: Change to attribute
+    pub graph: Mutex<graph::Graph<NodeInfo, u32>>, //TODO: Change to edge attribute
+}
+
+impl Dag {
+    fn add_node_with_topic_edges(
+        &self,
+        subscribe_topic_names: &[Cow<'static, str>],
+        publish_topic_names: &[Cow<'static, str>],
+    ) -> NodeIndex {
+        let add_node_info = NodeInfo {
+            task_id: 0, // Temporary task_id
+            subscribe_topics: subscribe_topic_names.to_vec(),
+            publish_topics: publish_topic_names.to_vec(),
+        };
+
+        let mut node = MCSNode::new();
+        let mut graph = self.graph.lock(&mut node);
+        let add_node_idx = graph.add_node(add_node_info);
+
+        for node_idx in graph.node_indices() {
+            if let Some(node_info) = graph.node_weight(node_idx) {
+                let subscribe_match = subscribe_topic_names
+                    .iter()
+                    .any(|sub| node_info.publish_topics.contains(sub));
+
+                let publish_match = publish_topic_names
+                    .iter()
+                    .any(|pub_| node_info.subscribe_topics.contains(pub_));
+
+                if subscribe_match {
+                    graph.add_edge(node_idx, add_node_idx, 0);
+                }
+
+                if publish_match {
+                    graph.add_edge(add_node_idx, node_idx, 0);
+                }
+            }
+        }
+
+        add_node_idx
+    }
+
+    pub async fn spawn_reactor<F, Args, Ret>(
+        &self,
+        reactor_name: Cow<'static, str>,
+        f: F,
+        subscribe_topic_names: Vec<Cow<'static, str>>,
+        publish_topic_names: Vec<Cow<'static, str>>,
+        sched_type: SchedulerType,
+    ) where
+        F: Fn(
+                <Args::Subscribers as MultipleReceiver>::Item,
+            ) -> <Ret::Publishers as MultipleSender>::Item
+            + Send
+            + 'static,
+        Args: VectorToSubscribers,
+        Ret: VectorToPublishers,
+        Ret::Publishers: Send,
+        Args::Subscribers: Send,
+    {
+        let node_idx = self.add_node_with_topic_edges(&subscribe_topic_names, &publish_topic_names);
+
+        let mut node = MCSNode::new();
+        let mut pending_tasks = PENDING_TASKS.lock(&mut node);
+
+        pending_tasks
+            .entry(self.id)
+            .or_default()
+            .push(PendingTask::new(node_idx, move || {
+                Box::pin(async move {
+                    spawn_reactor::<F, Args, Ret>(
+                        reactor_name.clone(),
+                        f,
+                        subscribe_topic_names.clone(),
+                        publish_topic_names.clone(),
+                        sched_type,
+                    )
+                    .await
+                })
+            }));
+    }
+
+    pub async fn spawn_periodic_reactor<F, Ret>(
+        &self,
+        reactor_name: Cow<'static, str>,
+        f: F,
+        publish_topic_names: Vec<Cow<'static, str>>,
+        sched_type: SchedulerType,
+        period: Duration,
+    ) where
+        F: Fn() -> <Ret::Publishers as MultipleSender>::Item + Send + 'static,
+        Ret: VectorToPublishers,
+        Ret::Publishers: Send,
+    {
+        let node_idx = self.add_node_with_topic_edges(&Vec::new(), &publish_topic_names);
+
+        let mut node = MCSNode::new();
+        let mut pending_tasks = PENDING_TASKS.lock(&mut node);
+
+        pending_tasks
+            .entry(self.id)
+            .or_default()
+            .push(PendingTask::new(node_idx, move || {
+                Box::pin(async move {
+                    spawn_periodic_reactor::<F, Ret>(
+                        reactor_name.clone(),
+                        f,
+                        publish_topic_names.clone(),
+                        sched_type,
+                        period,
+                    )
+                    .await
+                })
+            }));
+    }
 }
 
 /// DAGs.
@@ -69,4 +226,42 @@ pub fn get_dag(id: u32) -> Option<Arc<Dag>> {
     dags.id_to_dag.get(&id).cloned()
 }
 
-// TODO: Implementation of API to build DAGs from Reactor
+fn validate_dag(dag: &Dag) -> Result<(), DagError> {
+    let mut pending_node = MCSNode::new();
+    if PENDING_TASKS.lock(&mut pending_node).get(&dag.id).is_none() {
+        return Err(DagError::MissingPendingTasks(dag.id));
+    }
+
+    let mut graph_node = MCSNode::new();
+    let graph = dag.graph.lock(&mut graph_node);
+    if connected_components(&*graph) != 1 {
+        return Err(DagError::NotWeaklyConnected(dag.id));
+    }
+    if is_cyclic_directed(&*graph) {
+        return Err(DagError::ContainsCycle(dag.id));
+    }
+    Ok(())
+}
+
+pub async fn finish_create_dags(dags: &[Arc<Dag>]) -> Result<(), DagError> {
+    for dag in dags {
+        validate_dag(dag)?;
+
+        let pending_tasks = {
+            let mut node = MCSNode::new();
+            let mut lock = PENDING_TASKS.lock(&mut node);
+            lock.remove(&dag.id).unwrap()
+        };
+
+        for task in pending_tasks {
+            let task_id = (task.spawn)().await;
+            let mut graph_node = MCSNode::new();
+            let mut graph = dag.graph.lock(&mut graph_node);
+            if let Some(node_info) = graph.node_weight_mut(task.node_idx) {
+                node_info.task_id = task_id;
+            }
+        }
+    }
+
+    Ok(())
+}
