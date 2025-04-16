4f77e2b5d06ca67dd85a68af69224f23ac291d71
veqcc <ryuta.kambe@tier4.jp>
2025-04-16 11:02:18 +0900

    add revision check in attach()
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- awkernel_drivers/src/pcie/virtio/virtio_net.rs
+++ awkernel_drivers/src/pcie/virtio/virtio_net.rs
@@ -8,6 +8,11 @@ pub fn match_device(vendor: u16, id: u16) -> bool {
 }
 
 pub fn attach(info: PCIeInfo) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeDeviceErr> {
+    // Non-transitional devices SHOULD have a PCI Revision ID of 1 or higher.
+    if info.get_revision_id() == 0 {
+        return Err(PCIeDeviceErr::RevisionIDMismatch);
+    }
+
     // TODO: Implement
     log::info!("TODO: VirtIO-net attach() is to be implemented");
     Ok(info.unknown_device())
