206e83e80423de0ef2cb8a160bc60f34bbf5e74d
veqcc <ryuta.kambe@tier4.jp>
2025-04-16 11:05:33 +0900

    fix minor
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- awkernel_drivers/src/pcie/virtio/virtio_net.rs
+++ awkernel_drivers/src/pcie/virtio/virtio_net.rs
@@ -7,8 +7,11 @@ pub fn match_device(vendor: u16, id: u16) -> bool {
     vendor == pcie_id::VIRTIO_VENDOR_ID && id == VIRTIO_NET_ID
 }
 
-pub fn attach(info: PCIeInfo) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeDeviceErr> {
-    // Initialize PCIeInfo
+pub fn attach(mut info: PCIeInfo) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeDeviceErr> {
+    // Non-transitional devices SHOULD have a PCI Revision ID of 1 or higher.
+    if info.get_revision_id() == 0 {
+        return Err(PCIeDeviceErr::RevisionIDMismatch);
+    }
 
     // Map the memory regions of MMIO.
     if let Err(e) = info.map_bar() {
@@ -18,11 +21,6 @@ pub fn attach(info: PCIeInfo) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeD
 
     // Read capabilities of PCIe.
     info.read_capability();
-  
-    // Non-transitional devices SHOULD have a PCI Revision ID of 1 or higher.
-    if info.get_revision_id() == 0 {
-        return Err(PCIeDeviceErr::RevisionIDMismatch);
-    }
 
     // TODO: Implement
     log::info!("TODO: VirtIO-net attach() is to be implemented");
