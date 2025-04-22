c762cde9576e5ab7f74bbbf0a176a54139390f86
koichiimai <kotty.0704@gmail.com>
2025-04-23 08:41:41 +0900

    WIP
    Signed-off-by: koichiimai <kotty.0704@gmail.com>

--- awkernel_lib/src/file.rs
+++ awkernel_lib/src/file.rs
@@ -59,10 +59,6 @@ pub fn init_filesystem() {
     *file_manager = Some(FileManager { fs });
 }
 
-//pub fn init_file_manager() {
-//FILE_MANAGER.call_once(|| Mutex::new(Some(FileManager::new())));
-//}
-
 impl IoBase for InMemoryDisk {
     type Error = InMemoryDiskError;
 }
@@ -149,5 +145,114 @@ impl fatfs::IoError for InMemoryDiskError {
 
     fn new_write_zero_error() -> Self {
         InMemoryDiskError::WriteZero
+    }
+}
+
+pub struct FileDescriptor {
+    // Note first_cluster is None if file is empty
+    first_cluster: Option<u32>,
+    // Note: if offset points between clusters current_cluster is the previous cluster
+    current_cluster: Option<u32>,
+    // current position in this file
+    offset: u32,
+    // file dir entry editor - None for root dir
+    entry: Option<fatfs::dir_entry::DirEntryEditor>,
+}
+
+pub enum FileManagerError {
+    OpenError,
+    CreateError,
+    ReadError,
+    WriteError,
+    SeekError,
+}
+
+impl FileDescriptor {
+    pub fn open_file(path: &str) -> Result<Self, FileManagerError> {
+        let file_manager = FILE_MANAGER.read();
+        let fs;
+        if let Some(fm) = &*file_manager {
+            fs = &fm.fs;
+        } else {
+            panic!("file_manager isn't initialized");
+        }
+
+        let root_dir = fs.root_dir();
+        let result;
+        if let Ok(file) = root_dir.open_file(path) {
+            result = Ok(FileDescriptor {
+                first_cluster: file.first_cluster,
+                current_cluster: file.current_cluster,
+                offset: file.offset,
+                entry: file.entry.clone(),
+            })
+        } else {
+            result = Err(FileManagerError::OpenError)
+        }
+        result
+    }
+
+    pub fn create_file(path: &str) -> Result<Self, FileManagerError> {
+        let file_manager = FILE_MANAGER.read();
+        let fs;
+        if let Some(fm) = &*file_manager {
+            fs = &fm.fs;
+        } else {
+            panic!("file_manager isn't initialized");
+        }
+
+        let root_dir = fs.root_dir();
+        let result;
+        if let Ok(file) = root_dir.create_file(path) {
+            result = Ok(FileDescriptor {
+                first_cluster: file.first_cluster,
+                current_cluster: file.current_cluster,
+                offset: file.offset,
+                entry: file.entry.clone(),
+            })
+        } else {
+            result = Err(FileManagerError::CreateError)
+        }
+        result
+    }
+
+    pub fn read_file(self, buf: &mut [u8]) -> Result<usize, FileManagerError> {
+        let mut file_manager = FILE_MANAGER.write();
+        let fs;
+        if let Some(fm) = &mut *file_manager {
+            fs = &fm.fs;
+        } else {
+            panic!("file_manager isn't initialized");
+        }
+
+        let mut file = fatfs::File {
+            first_cluster: self.first_cluster,
+            current_cluster: self.current_cluster,
+            offset: self.offset,
+            entry: self.entry,
+            fs,
+        };
+
+        file.read(buf).map_err(|_| FileManagerError::ReadError)
+    }
+
+    pub fn write_file(self, buf: &[u8]) -> Result<usize, FileManagerError> {
+        let mut file_manager = FILE_MANAGER.write();
+        let fs;
+        if let Some(fm) = &mut *file_manager {
+            fs = &fm.fs;
+        } else {
+            panic!("file_manager isn't initialized");
+        }
+
+        let mut file = fatfs::File {
+            first_cluster: self.first_cluster,
+            current_cluster: self.current_cluster,
+            offset: self.offset,
+            entry: self.entry,
+            fs,
+        };
+
+        return file.write(buf).map_err(|_| FileManagerError::WriteError);
     }
 }
