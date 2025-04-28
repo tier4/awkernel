5bc2b725ec2b105d76d87137c0e809d3e50060b3
Koichi Imai <koichi.imai.2@tier4.jp>
2025-04-28 10:18:33 +0900

    WIP
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- applications/awkernel_services/src/lib.rs
+++ applications/awkernel_services/src/lib.rs
@@ -3,6 +3,7 @@
 extern crate alloc;
 
 mod buffered_logger;
+mod filesystem_service;
 mod network_service;
 
 use core::time::Duration;
@@ -10,6 +11,7 @@ use core::time::Duration;
 const NETWORK_SERVICE_NAME: &str = "[Awkernel] network service";
 const BUFFERED_LOGGER_NAME: &str = "[Awkernel] buffered logger service";
 const DISPLAY_SERVICE_NAME: &str = "[Awkernel] display service";
+const FILESYSTEM_SERVICE_NAME: &str = "[Awkernel] filesystem service";
 
 pub async fn run() {
     awkernel_async_lib::spawn(
@@ -29,6 +31,13 @@ pub async fn run() {
     awkernel_async_lib::spawn(
         DISPLAY_SERVICE_NAME.into(),
         awkernel_display::run(),
+        awkernel_async_lib::scheduler::SchedulerType::FIFO,
+    )
+    .await;
+
+    awkernel_async_lib::spawn(
+        FILESYSTEM_SERVICE_NAME.into(),
+        filesystem_service::run(),
         awkernel_async_lib::scheduler::SchedulerType::FIFO,
     )
     .await;
