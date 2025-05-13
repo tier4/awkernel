14ad5a4a53f1d0080556d98457184c0dbf6d1816
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-05-13 19:14:04 +0900

    refactor: rename args
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- applications/rd_gen_to_dags/src/build_dag.rs
+++ applications/rd_gen_to_dags/src/build_dag.rs
@@ -38,17 +38,17 @@ fn create_pub_topics(dag_id: u32, node_id: u32, out_links: &[u32]) -> Vec<Cow<'s
 }
 
 macro_rules! setup_node_registration {
-    ($dag:expr, $node:expr) => {{
+    ($dag:expr, $node_data:expr) => {{
         let dag_id = $dag.get_id();
-        let node_id = $node.get_id();
-        let execution_time = $node.get_execution_time();
-        let in_links = $node.get_in_links();
-        let out_links = $node.get_out_links();
+        let node_id = $node_data.get_id();
+        let execution_time = $node_data.get_execution_time();
+        let in_links = $node_data.get_in_links();
+        let out_links = $node_data.get_out_links();
         let reactor_name = create_reactor_name(dag_id, node_id);
         let sub_topics = create_sub_topics(dag_id, node_id, &in_links);
         let pub_topics = create_pub_topics(dag_id, node_id, &out_links);
-        let period = $node.get_period();
-        let relative_deadline = $node.get_end_to_end_deadline();
+        let period = $node_data.get_period();
+        let relative_deadline = $node_data.get_end_to_end_deadline();
 
         (
             execution_time,
@@ -62,9 +62,9 @@ macro_rules! setup_node_registration {
 }
 
 macro_rules! register_source {
-    ($dag:expr, $node:expr, $sched_type:expr, $($T:ident),*) => {
+    ($dag:expr, $node_data:expr, $sched_type:expr, $($T:ident),*) => {
         {
-            let (execution_time, reactor_name, _, pub_topics, period, _) = setup_node_registration!($dag, $node);
+            let (execution_time, reactor_name, _, pub_topics, period, _) = setup_node_registration!($dag, $node_data);
 
             $dag.register_periodic_reactor::<_, ($($T,)*)>(
                 reactor_name.clone(),
@@ -86,24 +86,24 @@ macro_rules! register_source {
 
 async fn register_source_node(
     dag: Arc<Dag>,
-    node: &NodeData,
+    node_data: &NodeData,
     sched_type: SchedulerType,
 ) -> Result<(), LinkNumError> {
-    let out_links_len = node.get_out_links().len();
+    let out_links_len = node_data.get_out_links().len();
 
     match out_links_len {
         0 => unreachable!("`is_source()` ensures that `out_links_len` is >= 1"),
-        1 => register_source!(dag, node, sched_type, u64),
-        2 => register_source!(dag, node, sched_type, u64, u64),
-        3 => register_source!(dag, node, sched_type, u64, u64, u64),
-        _ => Err(LinkNumError::Input(dag.get_id(), node.get_id())),
+        1 => register_source!(dag, node_data, sched_type, u64),
+        2 => register_source!(dag, node_data, sched_type, u64, u64),
+        3 => register_source!(dag, node_data, sched_type, u64, u64, u64),
+        _ => Err(LinkNumError::Input(dag.get_id(), node_data.get_id())),
     }
 }
 
 macro_rules! register_sink {
-    ($dag:expr, $node:expr, $sched_type:expr, $($T:ident),*) => {
+    ($dag:expr, $node_data:expr, $sched_type:expr, $($T:ident),*) => {
         {
-            let (execution_time, reactor_name, sub_topics, _, _, relative_deadline) = setup_node_registration!($dag, $node);
+            let (execution_time, reactor_name, sub_topics, _, _, relative_deadline) = setup_node_registration!($dag, $node_data);
 
             $dag.register_sink_reactor::<_, ($($T,)*)>(
                 reactor_name.clone(),
@@ -122,24 +122,24 @@ macro_rules! register_sink {
 
 async fn register_sink_node(
     dag: Arc<Dag>,
-    node: &NodeData,
+    node_data: &NodeData,
     sched_type: SchedulerType,
 ) -> Result<(), LinkNumError> {
-    let in_links_len = node.get_in_links().len();
+    let in_links_len = node_data.get_in_links().len();
 
     match in_links_len {
         0 => unreachable!("`is_sink()` ensures that `in_links_len` is >= 1."),
-        1 => register_sink!(dag, node, sched_type, u64),
-        2 => register_sink!(dag, node, sched_type, u64, u64),
-        3 => register_sink!(dag, node, sched_type, u64, u64, u64),
-        _ => Err(LinkNumError::Output(dag.get_id(), node.get_id())),
+        1 => register_sink!(dag, node_data, sched_type, u64),
+        2 => register_sink!(dag, node_data, sched_type, u64, u64),
+        3 => register_sink!(dag, node_data, sched_type, u64, u64, u64),
+        _ => Err(LinkNumError::Output(dag.get_id(), node_data.get_id())),
     }
 }
 
 macro_rules! register_intermediate {
-    ($dag:expr, $node:expr, $sched_type:expr, $($T_in:ident),*; $($T_out:ident),*) => {
+    ($dag:expr, $node_data:expr, $sched_type:expr, $($T_in:ident),*; $($T_out:ident),*) => {
         {
-            let (execution_time, reactor_name, sub_topics, pub_topics, _, _) = setup_node_registration!($dag, $node);
+            let (execution_time, reactor_name, sub_topics, pub_topics, _, _) = setup_node_registration!($dag, $node_data);
 
             $dag.register_reactor::<_, ($($T_in,)*), ($($T_out,)*)>(
                 reactor_name.clone(),
@@ -160,27 +160,27 @@ macro_rules! register_intermediate {
 
 async fn register_intermediate_node(
     dag: Arc<Dag>,
-    node: &NodeData,
+    node_data: &NodeData,
     sched_type: SchedulerType,
 ) -> Result<(), LinkNumError> {
     let dag_id = dag.get_id();
-    let node_id = node.get_id();
-    let in_links_len = node.get_in_links().len();
-    let out_links_len = node.get_out_links().len();
+    let node_id = node_data.get_id();
+    let in_links_len = node_data.get_in_links().len();
+    let out_links_len = node_data.get_out_links().len();
 
     match (in_links_len, out_links_len) {
         (0, 0) => Err(LinkNumError::NoLink(dag_id, node_id)),
         (0, _) => unreachable!("Sink node must be registered by register_sink_node()"),
         (_, 0) => unreachable!("Source node must be registered by register_source_node()"),
-        (1, 1) => register_intermediate!(dag, node, sched_type, u64; u64),
-        (1, 2) => register_intermediate!(dag, node, sched_type, u64; u64, u64),
-        (1, 3) => register_intermediate!(dag, node, sched_type, u64; u64, u64, u64),
-        (2, 1) => register_intermediate!(dag, node, sched_type, u64, u64; u64),
-        (2, 2) => register_intermediate!(dag, node, sched_type, u64, u64; u64, u64),
-        (2, 3) => register_intermediate!(dag, node, sched_type, u64, u64; u64, u64, u64),
-        (3, 1) => register_intermediate!(dag, node, sched_type, u64, u64, u64; u64),
-        (3, 2) => register_intermediate!(dag, node, sched_type, u64, u64, u64; u64, u64),
-        (3, 3) => register_intermediate!(dag, node, sched_type, u64, u64, u64; u64, u64, u64),
+        (1, 1) => register_intermediate!(dag, node_data, sched_type, u64; u64),
+        (1, 2) => register_intermediate!(dag, node_data, sched_type, u64; u64, u64),
+        (1, 3) => register_intermediate!(dag, node_data, sched_type, u64; u64, u64, u64),
+        (2, 1) => register_intermediate!(dag, node_data, sched_type, u64, u64; u64),
+        (2, 2) => register_intermediate!(dag, node_data, sched_type, u64, u64; u64, u64),
+        (2, 3) => register_intermediate!(dag, node_data, sched_type, u64, u64; u64, u64, u64),
+        (3, 1) => register_intermediate!(dag, node_data, sched_type, u64, u64, u64; u64),
+        (3, 2) => register_intermediate!(dag, node_data, sched_type, u64, u64, u64; u64, u64),
+        (3, 3) => register_intermediate!(dag, node_data, sched_type, u64, u64, u64; u64, u64, u64),
         (i, o) if i > 3 && o > 3 => Err(LinkNumError::InOut(dag_id, node_id)),
         (i, _) if i > 3 => Err(LinkNumError::Input(dag_id, node_id)),
         (_, o) if o > 3 => Err(LinkNumError::Output(dag_id, node_id)),
