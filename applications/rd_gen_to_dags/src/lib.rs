25b84b2f2b08d208d0491c226c499cd0fcf398d8
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-05-27 15:35:04 +0900

    refactor: error log
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- applications/rd_gen_to_dags/src/lib.rs
+++ applications/rd_gen_to_dags/src/lib.rs
@@ -12,24 +12,21 @@ use build_dag::build_dag;
 
 // A newline is required at the end due to yaml_peg specification.
 // If you have multiple files, define a `const` for each.
-// TODO: to be removed after filesystem implementation.
-const DAG_FILE_0: &str = concat!(include_str!("../DAGs/dag_0.yaml"), "\n");
-// const DAG_FILE_1: &str = concat!(include_str!("../../../../DAGs/dag_1.yaml"), "\n");
+// const DAG_FILE_0: &str = concat!(include_str!("path/to/dag_0.yaml"), "\n");
+// const DAG_FILE_1: &str = concat!(include_str!("path/to/dag_1.yaml"), "\n");
 
 pub async fn run() {
     wait_millisec(1000);
 
-    let dag_files = [DAG_FILE_0];
+    let dag_files = [/*DAG_FILE_0, DAG_FILE_1*/];
 
     let dags_data = match parse_yaml::parse_dags(&dag_files) {
         Ok(data) => data,
         Err(e) => {
-            log::error!("Error parsing YAML: {e}");
+            log::error!("Failed to parse YAML: {e}");
             return;
         }
     };
-
-    let expected_dag_count = dags_data.len();
 
     let mut success_build_dags = vec![];
 
@@ -42,11 +39,6 @@ pub async fn run() {
                 log::error!("Failed to build DAG: {e}");
             }
         }
-    }
-
-    if success_build_dags.len() != expected_dag_count {
-        log::error!("Not all DAGs were built successfully.");
-        return;
     }
 
     match finish_create_dags(&success_build_dags).await {
