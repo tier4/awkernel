00ad5df3d7bfec705d1360d0a1b8d082053467d1
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2025-06-02 16:15:48 +0900

    refactor: deleted test_pubsub
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- userland/src/lib.rs
+++ userland/src/lib.rs
@@ -13,9 +13,6 @@ pub async fn main() -> Result<(), Cow<'static, str>> {
     #[cfg(feature = "test_network")]
     test_network::run().await; // test for network
 
-    #[cfg(feature = "test_pubsub")]
-    test_pubsub::run().await; // test for pubsub
-
     #[cfg(feature = "test_rpi_hal")]
     test_rpi_hal::run().await; // test for RPi HAL
 
