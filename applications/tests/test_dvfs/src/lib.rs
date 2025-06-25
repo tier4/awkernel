53ab58d9d58dbfe83bd3ca896783940123193ba0
Yuuki Takano <ytakanoster@gmail.com>
2025-06-25 18:16:01 +0900

    update test_dvfs
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- applications/tests/test_dvfs/src/lib.rs
+++ applications/tests/test_dvfs/src/lib.rs
@@ -7,38 +7,64 @@ use core::{
 
 use alloc::{format, vec::Vec};
 use array_macro::array;
-use awkernel_lib::dvfs::DesiredPerformance;
-use num_traits::abs;
+use awkernel_lib::{
+    dvfs::DesiredPerformance,
+    sync::{mcs::MCSNode, mutex::Mutex},
+};
 
 extern crate alloc;
 
 mod nbody;
 
-const APP_NAME: &str = "test DVFS";
 const NUM_CPU: usize = 14;
-const NUM_TRIALS: usize = 100;
+const NUM_TRIALS_LATENCY: usize = 100;
 const NUM_BUSY_LOOP: usize = 1000000000;
 
-static LATENCY: [[[AtomicU64; NUM_TRIALS]; 11]; NUM_CPU] =
-    array![_ => array![_ => array![_ => AtomicU64::new(0); NUM_TRIALS]; 11]; NUM_CPU];
+static LATENCY: [[[AtomicU64; NUM_TRIALS_LATENCY]; 11]; NUM_CPU] =
+    array![_ => array![_ => array![_ => AtomicU64::new(0); NUM_TRIALS_LATENCY]; 11]; NUM_CPU];
 
 static COUNT: [[AtomicUsize; 11]; NUM_CPU] =
     array![_ => array![_ => AtomicUsize::new(0); 11]; NUM_CPU];
 static TOTAL_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub async fn run() {
+    let mut waiter = Vec::with_capacity(awkernel_lib::cpu::num_cpu() - 2);
+
     for _ in 0..(awkernel_lib::cpu::num_cpu() - 2) {
-        awkernel_async_lib::spawn(
-            APP_NAME.into(),
-            test_freq_latency(),
+        let w = awkernel_async_lib::spawn(
+            "test_latency_diff".into(),
+            test_latency_diff(),
             awkernel_async_lib::scheduler::SchedulerType::FIFO,
         )
         .await;
+
+        waiter.push(w);
+    }
+
+    for w in waiter {
+        let _ = w.join().await;
+    }
+
+    let mut waiter = Vec::with_capacity(awkernel_lib::cpu::num_cpu() - 2);
+
+    for _ in 0..(awkernel_lib::cpu::num_cpu() - 2) {
+        let w = awkernel_async_lib::spawn(
+            "test_latency".into(),
+            test_latency(),
+            awkernel_async_lib::scheduler::SchedulerType::FIFO,
+        )
+        .await;
+
+        waiter.push(w);
+    }
+
+    for w in waiter {
+        let _ = w.join().await;
     }
 }
 
-async fn test_dvfs() {
-    let end_count = (awkernel_lib::cpu::num_cpu() - 1) * NUM_TRIALS * 11;
+async fn test_latency() {
+    let end_count = (awkernel_lib::cpu::num_cpu() - 1) * NUM_TRIALS_LATENCY * 11;
 
     while TOTAL_COUNT.load(Ordering::Relaxed) + 1 < end_count {
         let cpu_id = awkernel_lib::cpu::cpu_id();
@@ -60,7 +86,7 @@ async fn test_dvfs() {
 
             let count =
                 COUNT[cpu_id][i as usize].fetch_add(1, core::sync::atomic::Ordering::Relaxed);
-            if count < NUM_TRIALS {
+            if count < NUM_TRIALS_LATENCY {
                 LATENCY[cpu_id][i as usize][count].store(
                     elapsed.as_micros() as u64,
                     core::sync::atomic::Ordering::Relaxed,
@@ -94,7 +120,7 @@ fn workload() -> Duration {
 
 fn print_latency() {
     let mut result: [[Vec<u64>; 11]; NUM_CPU] =
-        array![_ => array![_ => Vec::with_capacity(NUM_TRIALS); 11]; NUM_CPU];
+        array![_ => array![_ => Vec::with_capacity(NUM_TRIALS_LATENCY); 11]; NUM_CPU];
 
     for (j, latency_cpu) in LATENCY.iter().enumerate() {
         for (k, latency) in latency_cpu.iter().enumerate() {
@@ -113,7 +139,7 @@ fn print_latency() {
 
                 result[j][k].push(val);
             }
-            let avg = sum / NUM_TRIALS as u64;
+            let avg = sum / NUM_TRIALS_LATENCY as u64;
 
             let msg = format!(
                 "CPU {j}: Performance {}: Average: {avg} us, Min: {min} us, Max: {max} us\r\n",
@@ -128,22 +154,28 @@ fn print_latency() {
     awkernel_lib::console::print(&result_str);
 }
 
-async fn test_freq_latency() {
+const NUM_TRIALS_LATENCY_DIFF: usize = 20;
+static FREQ_LATENCY: [[Mutex<Vec<(u64, i64)>>; NUM_TRIALS_LATENCY_DIFF]; NUM_CPU] =
+    array![_ => array![_ => Mutex::new(Vec::new()); NUM_TRIALS_LATENCY_DIFF]; NUM_CPU];
+static TOTAL_COUNT_LATENCY_DIFF: AtomicUsize = AtomicUsize::new(0);
+static N: usize = 500;
+
+async fn test_latency_diff() {
     loop {
-        awkernel_lib::dvfs::set_min_max_performance(25);
+        awkernel_lib::dvfs::set_min_max_performance(10);
         awkernel_lib::dvfs::set_energy_efficiency(0);
         awkernel_lib::dvfs::set_desired_performance(DesiredPerformance::Auto);
 
-        warm_up();
+        workload();
 
-        let mut diff = Vec::with_capacity(100000);
+        let mut diff = Vec::with_capacity(N);
 
         awkernel_lib::dvfs::set_min_max_performance(100);
         awkernel_lib::dvfs::set_energy_efficiency(0);
         awkernel_lib::dvfs::set_desired_performance(DesiredPerformance::Auto);
 
         let t = awkernel_async_lib::time::Time::now();
-        for _ in 0..100000 {
+        for _ in 0..N {
             let start = unsafe { core::arch::x86_64::_rdtsc() };
             fence(Ordering::AcqRel);
             for _ in 0..1000 {
@@ -154,15 +186,56 @@ async fn test_freq_latency() {
             diff.push((t.elapsed(), (end - start) as i64));
         }
 
-        // let mut result = Vec::new();
+        let mut result = Vec::new();
 
-        for i in 0..100 {
-            // result.push((diff[i].0, diff[i + 1].1 - diff[i].1));
-            if abs(diff[i + 1].1 - diff[i].1) > 100 {
-                log::debug!("{i}: {:?}", diff[i + 1].1 - diff[i].1);
+        for i in 0..(N - 1) {
+            result.push((diff[i].0.as_nanos() as u64, diff[i + 1].1 - diff[i].1));
+        }
+
+        let cpu_id = awkernel_lib::cpu::cpu_id();
+        for (i, r) in FREQ_LATENCY[cpu_id].iter().enumerate() {
+            let mut node = MCSNode::new();
+            let mut guard = r.lock(&mut node);
+            if guard.is_empty() {
+                *guard = result;
+                drop(guard);
+
+                let old_total = TOTAL_COUNT_LATENCY_DIFF.fetch_add(1, Ordering::Relaxed);
+
+                log::debug!("{cpu_id}: {i}, {old_total}");
+
+                if old_total == (NUM_CPU - 1) * NUM_TRIALS_LATENCY_DIFF - 1 {
+                    print_latency_diff();
+                }
+
+                break;
             }
         }
 
-        // log::debug!("{result:?}");
+        let total = TOTAL_COUNT_LATENCY_DIFF.load(Ordering::Relaxed);
+
+        if total == (NUM_CPU - 1) * NUM_TRIALS_LATENCY_DIFF {
+            break;
+        }
+
+        awkernel_async_lib::r#yield().await;
     }
+}
+
+fn print_latency_diff() {
+    let mut result: [[Vec<(u64, i64)>; NUM_TRIALS_LATENCY_DIFF]; NUM_CPU] =
+        array![_ => array![_ => Vec::new(); NUM_TRIALS_LATENCY_DIFF]; NUM_CPU];
+
+    for (dst, src) in result.iter_mut().zip(FREQ_LATENCY.iter()) {
+        for (dst, src) in dst.iter_mut().zip(src.iter()) {
+            let mut node = MCSNode::new();
+            let guard = src.lock(&mut node);
+
+            *dst = guard.clone();
+        }
+    }
+
+    let result_json = serde_json::to_string(&result).unwrap();
+    let result_str = format!("{result_json}\r\n");
+    awkernel_lib::console::print(&result_str);
 }
