5eaa1f7e3e60f8bebbb6bdc0b218de290f2acdf6
Koichi Imai <koichi.imai.2@tier4.jp>
2025-07-01 17:42:54 +0900

    fix comment
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- awkernel_async_lib/src/file/path.rs
+++ awkernel_async_lib/src/file/path.rs
@@ -1,4 +1,4 @@
-//! Virtual filesystem path (async version)
+//! Async Virtual filesystem path
 //!
 //! The virtual file system abstraction generalizes over file systems and allow using
 //! different VirtualFileSystem implementations (i.e. an in memory implementation for unit tests)
