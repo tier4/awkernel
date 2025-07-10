1eaf06a53ba5680e13a53608cfc64d55e94f9307
Koichi Imai <koichi.imai.2@tier4.jp>
2025-07-10 21:49:30 +0900

    add Nvme new()
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- awkernel_drivers/src/pcie/nvme.rs
+++ awkernel_drivers/src/pcie/nvme.rs
@@ -1,7 +1,123 @@
-use alloc::sync::Arc;
-
 use super::{PCIeDevice, PCIeDeviceErr, PCIeInfo};
+use alloc::{format, sync::Arc};
+use awkernel_lib::sync::rwlock::RwLock;
 
-pub(super) fn attach(info: PCIeInfo) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeDeviceErr> {
-    Ok(info.unknown_device())
+const DEVICE_NAME: &str = "NVMe Controller";
+const _DEVICE_SHORT_NAME: &str = "nvme";
+
+pub(super) fn attach(
+    mut info: PCIeInfo,
+) -> Result<Arc<dyn PCIeDevice + Sync + Send>, PCIeDeviceErr> {
+    // Initialize PCIeInfo
+
+    // Map the memory regions of MMIO.
+    if let Err(e) = info.map_bar() {
+        log::warn!("NVMe: Failed to map the memory regions of MMIO: {e:?}");
+        return Err(PCIeDeviceErr::PageTableFailure);
+    }
+
+    // Read capabilities of PCIe.
+    info.read_capability();
+
+    let nvme = Nvme::new(info)?;
+
+    Ok(Arc::new(nvme))
+}
+struct NvmeInner {
+    info: PCIeInfo,
+}
+impl NvmeInner {
+    fn new(info: PCIeInfo) -> Result<Self, NvmeDriverErr> {
+        log::info!("NVMe: Initializing NVMe controller");
+        Ok(Self { info })
+    }
+}
+
+struct Nvme {
+    inner: RwLock<NvmeInner>,
+}
+impl Nvme {
+    fn new(info: PCIeInfo) -> Result<Self, PCIeDeviceErr> {
+        let inner = NvmeInner::new(info)?;
+
+        let nvme = Self {
+            inner: RwLock::new(inner),
+        };
+
+        Ok(nvme)
+    }
+}
+
+impl PCIeDevice for Nvme {
+    fn device_name(&self) -> alloc::borrow::Cow<'static, str> {
+        let inner = self.inner.read();
+        let bfd = inner.info.get_bfd();
+        let name = format!("{bfd}:{DEVICE_NAME}");
+        name.into()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum NvmeDriverErr {
+    InitFailure,
+    NoBar0,
+    ReadFailure,
+    DMAPool,
+    NotReady,
+    NotImplemented,
+    NoAdminQueue,
+    CommandTimeout,
+    CommandFailed,
+    NoCcb,
+    IncompatiblePageSize,
+}
+
+impl From<NvmeDriverErr> for PCIeDeviceErr {
+    fn from(value: NvmeDriverErr) -> Self {
+        log::error!("nvme: {value:?}");
+        match value {
+            NvmeDriverErr::InitFailure => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::NoBar0 => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::ReadFailure => PCIeDeviceErr::ReadFailure,
+            NvmeDriverErr::NotReady => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::DMAPool => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::NotImplemented => PCIeDeviceErr::NotImplemented,
+            NvmeDriverErr::NoAdminQueue => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::CommandTimeout => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::CommandFailed => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::NoCcb => PCIeDeviceErr::InitFailure,
+            NvmeDriverErr::IncompatiblePageSize => PCIeDeviceErr::InitFailure,
+        }
+    }
+}
+
+#[inline(always)]
+pub fn write_reg(info: &PCIeInfo, offset: usize, value: u32) -> Result<(), NvmeDriverErr> {
+    let mut bar0 = info.get_bar(0).ok_or(NvmeDriverErr::NoBar0)?;
+    bar0.write32(offset, value);
+    Ok(())
+}
+
+#[inline(always)]
+pub fn write_reg_array(
+    info: &PCIeInfo,
+    offset: usize,
+    index: usize,
+    value: u32,
+) -> Result<(), NvmeDriverErr> {
+    let mut bar0 = info.get_bar(0).ok_or(NvmeDriverErr::NoBar0)?;
+    bar0.write32(offset + (index << 2), value);
+    Ok(())
+}
+#[inline(always)]
+pub fn read_reg(info: &PCIeInfo, offset: usize) -> Result<u32, NvmeDriverErr> {
+    let bar0 = info.get_bar(0).ok_or(NvmeDriverErr::NoBar0)?;
+    bar0.read32(offset).ok_or(NvmeDriverErr::ReadFailure)
+}
+
+#[inline(always)]
+pub fn read_reg_array(info: &PCIeInfo, offset: usize, index: usize) -> Result<u32, NvmeDriverErr> {
+    let bar0 = info.get_bar(0).ok_or(NvmeDriverErr::NoBar0)?;
+    bar0.read32(offset + (index << 2))
+        .ok_or(NvmeDriverErr::ReadFailure)
 }
