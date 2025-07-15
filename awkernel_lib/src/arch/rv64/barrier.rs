80ad8dca49ea9d155d3cb38d9dae5e6af3f512ef
Koichi Imai <45482193+Koichi98@users.noreply.github.com>
2025-07-15 11:22:01 +0900

    feat(barrier): add barrier trait (#586)
    * add disable()
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * delete log
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * define barrier as trait
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * delete nvme changes
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * fix
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * cargo fmt
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    
    * delete comments
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    * delete comments
    
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>
    Signed-off-by: Koichi Imai <koichi.imai.2@tier4.jp>

--- awkernel_lib/src/arch/rv64/barrier.rs
+++ awkernel_lib/src/arch/rv64/barrier.rs
@@ -5,52 +5,46 @@ use core::arch::asm;
 pub const BUS_SPACE_BARRIER_READ: u32 = 0x01;
 pub const BUS_SPACE_BARRIER_WRITE: u32 = 0x02;
 
-/// Enter critical section memory barrier
-#[inline(always)]
-pub fn membar_enter() {
-    unsafe {
-        asm!("fence w,rw");
+impl crate::barrier::Barrier for super::RV64 {
+    #[inline(always)]
+    fn membar_enter() {
+        unsafe {
+            asm!("fence w,rw");
+        }
     }
-}
 
-/// Exit critical section memory barrier
-#[inline(always)]
-pub fn membar_exit() {
-    unsafe {
-        asm!("fence rw,w");
+    #[inline(always)]
+    fn membar_exit() {
+        unsafe {
+            asm!("fence rw,w");
+        }
     }
-}
 
-/// Producer memory barrier - ensures all stores before the barrier complete
-/// before any stores after the barrier.
-#[inline(always)]
-pub fn membar_producer() {
-    unsafe {
-        asm!("fence w,w");
+    #[inline(always)]
+    fn membar_producer() {
+        unsafe {
+            asm!("fence w,w");
+        }
     }
-}
 
-/// Consumer memory barrier - ensures all loads before the barrier complete
-/// before any loads after the barrier.
-#[inline(always)]
-pub fn membar_consumer() {
-    unsafe {
-        asm!("fence r,r");
+    #[inline(always)]
+    fn membar_consumer() {
+        unsafe {
+            asm!("fence r,r");
+        }
     }
-}
 
-/// Full memory synchronization barrier
-#[inline(always)]
-pub fn membar_sync() {
-    unsafe {
-        asm!("fence rw,rw");
+    #[inline(always)]
+    fn membar_sync() {
+        unsafe {
+            asm!("fence rw,rw");
+        }
     }
-}
 
-/// Bus space barrier - ensures ordering of MMIO operations
-#[inline(always)]
-pub fn bus_space_barrier(_flags: u32) {
-    unsafe {
-        asm!("fence iorw,iorw");
+    #[inline(always)]
+    fn bus_space_barrier(_flags: u32) {
+        unsafe {
+            asm!("fence iorw,iorw");
+        }
     }
 }
