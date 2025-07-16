cb7a8cc68954929ffe7d3180f04de490cd05c1c8
Yuuki Takano <ytakanoster@gmail.com>
2025-07-16 10:20:26 +0900

    use time command
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- specification/awkernel_async_lib/src/task/wake_workers_no_std/run.sh
+++ specification/awkernel_async_lib/src/task/wake_workers_no_std/run.sh
@@ -3,10 +3,10 @@ rm -f result.txt
 for i in $(seq 0 3); do
     echo "IRQ_POS$i"
 
-    # Level-triggered timer
+    # Level-sensitive timer
     make clean
     echo make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_LEVEL run
-    make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_LEVEL run | tee concurrent_work_conservation_level_irq$i.out
+    time make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_LEVEL run | tee concurrent_work_conservation_level_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_LEVEL run" >> result.txt
@@ -18,7 +18,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_LEVEL run
-    make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_LEVEL run | tee cpu_waking_to_active_cpu0_level_irq$i.out
+    time make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_LEVEL run | tee cpu_waking_to_active_cpu0_level_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_LEVEL run" >> result.txt
@@ -30,7 +30,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_LEVEL run
-    make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_LEVEL run | tee cpu_waking_to_active_cpu1_level_irq$i.out
+    time make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_LEVEL run | tee cpu_waking_to_active_cpu1_level_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_LEVEL run" >> result.txt
@@ -42,7 +42,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_LEVEL run
-    make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_LEVEL run | tee eventually_execute_level_irq$i.out
+    time make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_LEVEL run | tee eventually_execute_level_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_LEVEL run" >> result.txt
@@ -55,7 +55,7 @@ for i in $(seq 0 3); do
     # Edge-triggered timer
     make clean
     echo make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_EDGE run
-    make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_EDGE run | tee concurrent_work_conservation_edge_irq$i.out
+    time make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_EDGE run | tee concurrent_work_conservation_edge_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CONCURRENT_WORK_CONSERVATION TIMER=TIMER_EDGE run" >> result.txt
@@ -67,7 +67,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_EDGE run
-    make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_EDGE run | tee cpu_waking_to_active_cpu0_edge_irq$i.out
+    time make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_EDGE run | tee cpu_waking_to_active_cpu0_edge_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU0 TIMER=TIMER_EDGE run" >> result.txt
@@ -79,7 +79,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_EDGE run
-    make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_EDGE run | tee cpu_waking_to_active_cpu1_edge_irq$i.out
+    time make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_EDGE run | tee cpu_waking_to_active_cpu1_edge_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=CPU_WAKING_TO_ACTIVE_CPU1 TIMER=TIMER_EDGE run" >> result.txt
@@ -91,7 +91,7 @@ for i in $(seq 0 3); do
 
     make clean
     echo make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_EDGE run
-    make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_EDGE run | tee eventually_execute_edge_irq$i.out
+    time make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_EDGE run | tee eventually_execute_edge_irq$i.out
 
     if [ -e "wake_workers.pml.trail" ]; then
         echo "false make IRQ_POS=$i TARGET=EVENTUALLY_EXECUTE TIMER=TIMER_EDGE run" >> result.txt
