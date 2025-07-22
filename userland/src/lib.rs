d3c056bd47a1e90c433f8237c1ce4ae31767aa87
Koichi <koichi.imai.2@tier4.jp>
2025-07-22 11:53:01 +0900

    add test mount table
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- userland/src/lib.rs
+++ userland/src/lib.rs
@@ -52,5 +52,8 @@ pub async fn main() -> Result<(), Cow<'static, str>> {
     #[cfg(feature = "test_voluntary_preemption")]
     test_voluntary_preemption::run().await; // test for voluntary preemption
 
+    #[cfg(feature = "test_mount_table")]
+    test_mount_table::run().await; // test for mount table
+
     Ok(())
 }
