118ac769363fb44f03c45d8633267b48aa605153
Koichi <koichi.imai.2@tier4.jp>
2025-07-30 14:58:49 +0900

    comment out nvme device driver attach
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- awkernel_drivers/src/pcie.rs
+++ awkernel_drivers/src/pcie.rs
@@ -36,7 +36,7 @@ pub mod broadcom;
 mod capability;
 mod config_space;
 pub mod intel;
-pub mod nvme;
+//pub mod nvme;
 pub mod pcie_class;
 pub mod pcie_id;
 pub mod raspi;
@@ -1013,12 +1013,12 @@ impl PCIeInfo {
                 return virtio::attach(self);
             }
             _ => {
-                if let PCIeClass::MassStorageController(pcie_class::PCIeStorageSubClass::Nvm(
-                    pcie_class::PCIeStorageNvmProgrammingInterface::NvmExpressIOController,
-                )) = self.get_class()
-                {
-                    return nvme::attach(self);
-                }
+                //if let PCIeClass::MassStorageController(pcie_class::PCIeStorageSubClass::Nvm(
+                //pcie_class::PCIeStorageNvmProgrammingInterface::NvmExpressIOController,
+                //)) = self.get_class()
+                //{
+                //return nvme::attach(self);
+                //}
             }
         }
 
