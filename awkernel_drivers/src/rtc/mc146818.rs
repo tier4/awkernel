f75591ba934173cb8831982bd236def0d4793e70
Koichi <koichi.imai.2@tier4.jp>
2025-08-12 13:45:00 +0900

    add comments
    Signed-off-by: Koichi <koichi.imai.2@tier4.jp>

--- awkernel_drivers/src/rtc/mc146818.rs
+++ awkernel_drivers/src/rtc/mc146818.rs
@@ -184,7 +184,7 @@ impl Mc146818Rtc {
         regs[MC_YEAR as usize] = Self::bintobcd((time.year % 100) as u8);
 
         // Day of week (1-7) - not calculated currently, just using placeholder
-        // TODO: Implement day of week calculation if needed
+        // NOTE: Implement day of week calculation if needed
         regs[MC_DOW as usize] = 1;
 
         regs[7] = 0;
