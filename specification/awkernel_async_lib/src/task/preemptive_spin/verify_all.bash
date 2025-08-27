bbc9232c64e866bf68bad3c8ef9da3da16c7c943
atsushi421 <atsushi.yano.2@tier4.jp>
2025-08-28 08:50:19 +0900

    fix

--- specification/awkernel_async_lib/src/task/preemptive_spin/verify_all.bash
+++ specification/awkernel_async_lib/src/task/preemptive_spin/verify_all.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# LTLS=(ensure_priority eventually_prerequisites eventually_terminate)
-LTLS=(ensure_priority)
-# SCHED_TYPE_PATTERNS=(0 1 2 3)
-SCHED_TYPE_PATTERNS=(1)
+LTLS=(ensure_priority eventually_prerequisites eventually_terminate)
+SCHED_TYPE_PATTERNS=(0 1 2 3)
 
 for sched_type_pattern in ${SCHED_TYPE_PATTERNS[@]}; do
     for ltl in ${LTLS[@]}; do
@@ -11,7 +9,7 @@ for sched_type_pattern in ${SCHED_TYPE_PATTERNS[@]}; do
         echo "Verifying with LTL=$ltl and sched_type_pattern=$sched_type_pattern"
         echo "==============================================================="
 
-        rm -f *.trail
+        rm -f pan.* *.trail pan *.tmp
         spin -DSCHED_TYPE_PATTERN=$sched_type_pattern -a preemptive.pml
         gcc -O3 -DCOLLAPSE -DVECTORSZ=5000 -DNFAIR=3 -o pan pan.c
         ./pan -a -n -m30000 -N $ltl
