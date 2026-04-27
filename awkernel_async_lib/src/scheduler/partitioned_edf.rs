41a20f0e1ea61cb4c339a9f22f404841e3b68549
Yuuki Takano <ytakanoster@gmail.com>
2026-04-27 17:38:21 +0900

    fix bugs
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- awkernel_async_lib/src/scheduler/partitioned_edf.rs
+++ awkernel_async_lib/src/scheduler/partitioned_edf.rs
@@ -1,12 +1,13 @@
-//! A GEDF scheduler.
+//! A Partitioned EDF scheduler.
 
-use core::sync::atomic::Ordering;
+use core::{cmp::max, sync::atomic::Ordering};
 
 use super::{Scheduler, SchedulerType, Task};
 use crate::{
     dag::{get_dag, get_dag_absolute_deadline, set_dag_absolute_deadline, to_node_index},
     scheduler::{
-        get_priority, peek_preemption_pending, push_preemption_pending, GLOBAL_WAKE_GET_MUTEX,
+        gedf::calculate_and_update_dag_deadline, get_priority, peek_preemption_pending,
+        push_preemption_pending, GLOBAL_WAKE_GET_MUTEX,
     },
     task::{
         get_task, get_task_running, set_current_task, set_need_preemption, DagInfo, State,
@@ -72,7 +73,7 @@ impl Scheduler for PartitionedGEDFScheduler {
             let mut info = task.info.lock(&mut node_inner);
             let dag_info = info.get_dag_info();
             match info.scheduler_type {
-                SchedulerType::PartitionedGEDF(relative_deadline, paritioned_core) => {
+                SchedulerType::PartitionedGEDF(relative_deadline, partitioned_core) => {
                     let wake_time = awkernel_lib::delay::uptime();
                     let absolute_deadline = if let Some(ref dag_info) = dag_info {
                         calculate_and_update_dag_deadline(dag_info, wake_time)
@@ -86,7 +87,7 @@ impl Scheduler for PartitionedGEDFScheduler {
                         .update_priority_info(self.priority, MAX_TASK_PRIORITY - absolute_deadline);
                     info.update_absolute_deadline(absolute_deadline);
 
-                    (wake_time, absolute_deadline, paritioned_core)
+                    (wake_time, absolute_deadline, partitioned_core)
                 }
                 _ => unreachable!(),
             }
@@ -178,53 +179,28 @@ impl PartitionedGEDFScheduler {
         }
 
         let task_running = get_task(task_running.task_id);
-        if let Some(target_task) = peek_preemption_pending(cpu_id).or(task_running) {
-            if task > target_task {
-                push_preemption_pending(cpu_id, task);
-                let preempt_irq = awkernel_lib::interrupt::get_preempt_irq();
-                set_need_preemption(target_task.id, cpu_id);
-                awkernel_lib::interrupt::send_ipi(preempt_irq, cpu_id as u32);
+        let task_pending = peek_preemption_pending(cpu_id);
 
-                // NOTE(atsushi421): Currently, preemption is requested regardless of the number of idle CPUs.
-                // While this implementation easily prevents priority inversion, it may also cause unnecessary preemption.
-                // Therefore, a more sophisticated implementation will be considered in the future.
+        let target_task = match (task_running, task_pending) {
+            (Some(running), Some(pending)) => max(running, pending),
+            (Some(running), None) => running,
+            (None, Some(pending)) => pending,
+            (None, None) => return false,
+        };
 
-                return true;
-            }
+        if task > target_task {
+            push_preemption_pending(cpu_id, task);
+            let preempt_irq = awkernel_lib::interrupt::get_preempt_irq();
+            set_need_preemption(target_task.id, cpu_id);
+            awkernel_lib::interrupt::send_ipi(preempt_irq, cpu_id as u32);
+
+            // NOTE(atsushi421): Currently, preemption is requested regardless of the number of idle CPUs.
+            // While this implementation easily prevents priority inversion, it may also cause unnecessary preemption.
+            // Therefore, a more sophisticated implementation will be considered in the future.
+
+            return true;
         }
 
         false
     }
-}
-
-fn get_dag_sink_relative_deadline_ms(dag_id: u32) -> u64 {
-    let dag = get_dag(dag_id).unwrap_or_else(|| panic!("GEDF scheduler: DAG {dag_id} not found"));
-    dag.get_sink_relative_deadline()
-        .map(|deadline| deadline.as_millis() as u64)
-        .unwrap_or_else(|| panic!("GEDF scheduler: DAG {dag_id} has no sink relative deadline set"))
-}
-
-fn calculate_and_set_dag_deadline(dag_id: u32, wake_time: u64) -> u64 {
-    let relative_deadline_ms = get_dag_sink_relative_deadline_ms(dag_id);
-    let dag_absolute_deadline = wake_time + relative_deadline_ms;
-    set_dag_absolute_deadline(dag_id, dag_absolute_deadline);
-    dag_absolute_deadline
-}
-
-fn calculate_and_update_dag_deadline(dag_info: &DagInfo, wake_time: u64) -> u64 {
-    let dag_id = dag_info.dag_id;
-    let node_id = dag_info.node_id;
-
-    if let Some(absolute_deadline) = get_dag_absolute_deadline(dag_id) {
-        let dag =
-            get_dag(dag_id).unwrap_or_else(|| panic!("GEDF scheduler: DAG {dag_id} not found"));
-        let current_node_index = to_node_index(node_id);
-        if !dag.is_source_node(current_node_index) {
-            return absolute_deadline;
-        }
-
-        return calculate_and_set_dag_deadline(dag_id, wake_time);
-    }
-
-    calculate_and_set_dag_deadline(dag_id, wake_time)
 }
