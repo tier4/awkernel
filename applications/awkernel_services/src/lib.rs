992dae672b3bd85e02d32019aa7c3d7ed821fe4f
nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>
2026-05-08 22:49:56 +0900

    fix: reboot timing increased
    Signed-off-by: nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>

--- applications/awkernel_services/src/lib.rs
+++ applications/awkernel_services/src/lib.rs
@@ -10,6 +10,10 @@ use core::time::Duration;
 const NETWORK_SERVICE_NAME: &str = "[Awkernel] network service";
 const BUFFERED_LOGGER_NAME: &str = "[Awkernel] buffered logger service";
 const DISPLAY_SERVICE_NAME: &str = "[Awkernel] display service";
+const WATCHDOG_NAME: &str = "[Awkernel] watchdog";
+
+/// Auto-reboot after this many seconds. Set to 0 to disable.
+const WATCHDOG_REBOOT_SECS: u64 = 60;
 
 pub async fn run() {
     awkernel_async_lib::spawn(
@@ -33,6 +37,25 @@ pub async fn run() {
     )
     .await;
 
+    if WATCHDOG_REBOOT_SECS > 0 {
+        awkernel_async_lib::spawn(
+            WATCHDOG_NAME.into(),
+            watchdog_reboot(WATCHDOG_REBOOT_SECS),
+            awkernel_async_lib::scheduler::SchedulerType::PrioritizedFIFO(0),
+        )
+        .await;
+    }
+
     awkernel_async_lib::sleep(Duration::from_secs(1)).await;
     awkernel_shell::init();
+}
+
+/// Sleep for the given number of seconds and then reboot.
+async fn watchdog_reboot(secs: u64) {
+    log::info!("Watchdog: will reboot in {secs} seconds");
+    awkernel_async_lib::sleep(Duration::from_secs(secs)).await;
+    log::info!("Watchdog: rebooting now after {secs} seconds");
+
+    #[cfg(target_arch = "x86_64")]
+    awkernel_lib::arch::x86_64::power::reboot();
 }
