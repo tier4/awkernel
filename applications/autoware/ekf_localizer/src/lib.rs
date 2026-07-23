98649e13cb26934e162a31928d6671588b056db2
nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>
2026-07-24 03:55:32 +0900

    fix: add licence
    Signed-off-by: nokosaaan <nishimura.r.019@ms.saitama-u.ac.jp>

--- applications/autoware/ekf_localizer/src/lib.rs
+++ applications/autoware/ekf_localizer/src/lib.rs
@@ -1,7 +1,23 @@
+// Copyright 2018-2019 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Ported from the following versions of the original C++ code:
 // core/autoware_core:
 // type: git
 // url: https://github.com/autowarefoundation/autoware_core.git
+// original file path: localization/autoware_ekf_localizer/src/ekf_localizer.cpp
+// test code: localization/autoware_ekf_localizer/test/test_ekf_module.cpp
 // version: 1.8.0
 
 #![no_std]
